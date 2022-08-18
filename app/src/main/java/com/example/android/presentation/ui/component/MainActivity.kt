53aa5f0b29e85dfea163501494c3b00b8327dcf0
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-08-18 23:34:05 +0900

    architecture setting

--- app/src/main/java/com/example/android/presentation/ui/component/MainActivity.kt
+++ app/src/main/java/com/example/android/presentation/ui/component/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.android
+package com.example.android.presentation.ui.component
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
