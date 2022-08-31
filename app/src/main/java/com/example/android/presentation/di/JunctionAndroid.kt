88d728b6e2d5e7088f000bb130ee1e8fb6979f66
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-08-31 16:03:46 +0900

    Compose로 UI변경

--- app/src/main/java/com/example/android/presentation/di/JunctionAndroid.kt
+++
@@ -1,7 +0,0 @@
-package com.example.android.presentation.di
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class JunctionAndroid: Application()
--- app/src/main/java/com/example/android/presentation/di/RepositoryModule.kt
+++
@@ -1,4 +0,0 @@
-package com.example.android.presentation.di
-
-object RepositoryModule {
-}
