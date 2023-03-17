408781f602935b7087a6004a0cf878936943e0c4
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-03-17 14:49:41 +0900

    buildSrc 세팅

---
+++ buildSrc/src/main/kotlin/Version.kt
@@ -0,0 +1,44 @@
+import org.gradle.api.JavaVersion
+
+object Version {
+    const val GRADLE_ANDROID = "7.0.4"
+    const val GRADLE_KOTLIN = "1.6.21"
+
+    val JAVA_VERSION = JavaVersion.VERSION_1_8
+    const val KOTLINX_COROUTINES = "1.6.0"
+
+    const val COMPILE_SDK_VERSION = 33
+    const val MIN_SDK_VERSION = 28
+    const val TARGET_SDK_VERSION = 33
+
+    const val CORE_KTX = "1.5.0"
+    const val LIFECYCLE_KTX = "2.4.0-alpha01"
+    const val APP_COMPAT = "1.6.0"
+    const val PREFERENCE_KTX = "1.2.0"
+
+    const val COMPOSE_ACTIVITY = "1.3.1"
+    const val COMPOSE = "1.2.0-rc01"
+    const val COMPOSE_MATERIAL = "1.2.0"
+    const val HILT_NAV = "1.0.0"
+
+    const val HILT = "2.39"
+
+    const val ROOM = "2.4.3"
+
+    const val ORBIT = "4.5.0"
+
+    const val RETROFIT = "2.9.0"
+    const val OKHTTP = "4.9.3"
+
+    const val JUNIT = "4.13.2"
+    const val MOCKITO_KOTLIN = "4.1.0"
+    const val MOCKITO_INLINE = "2.21.0"
+    const val ANDROID_JUNIT = "1.1.2"
+    const val ESPRESSO_CORE = "3.3.0"
+
+    const val COIL = "2.2.2"
+
+    const val EXO_PLAYER = "2.17.1"
+
+    const val INJECT = "1"
+}
