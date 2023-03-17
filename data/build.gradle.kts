0d5810c52461b8b34b617a2e64e908a977df28a7
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-03-17 14:54:34 +0900

    data 모듈 추가

---
+++ data/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.danbam.data"
+    compileSdk = Version.COMPILE_SDK_VERSION
+
+    defaultConfig {
+        minSdk = Version.MIN_SDK_VERSION
+        targetSdk = Version.TARGET_SDK_VERSION
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = Version.JAVA_VERSION
+        targetCompatibility = Version.JAVA_VERSION
+    }
+    kotlinOptions {
+        jvmTarget = Version.JAVA_VERSION.toString()
+    }
+}
+
+dependencies {
+    implementation(Dependency.JavaX.INJECT)
+
+    implementation(Dependency.Room.ROOM)
+    kapt(Dependency.Room.ROOM_COMPILER)
+
+    implementation(Dependency.Retrofit.RETROFIT)
+    implementation(Dependency.Retrofit.RETROFIT_CONVERTER_GSON)
+    implementation(Dependency.Retrofit.OKHTTP)
+    implementation(Dependency.Retrofit.OKHTTP_LOGGING_INTERCEPTOR)
+
+    testImplementation(Dependency.UnitTest.JUNIT)
+    testImplementation(Dependency.UnitTest.MOCKITO_KOTLIN)
+    testImplementation(Dependency.UnitTest.MOCKITO_INLINE)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,3 +14,4 @@ dependencyResolutionManagement {
 }
 rootProject.name = "indi_straw"
 include(":presentation")
+include(":data")
