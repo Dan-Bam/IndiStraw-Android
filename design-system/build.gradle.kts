e2bd25fc09555248fd7335d3a9c35f4b468d4eea
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-04-03 11:55:39 +0900

    디자인 시스템 모듈 추가

---
+++ design-system/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.ggjg.design_system"
+    compileSdk = Version.COMPILE_SDK_VERSION
+
+    defaultConfig {
+        minSdk = Version.MIN_SDK_VERSION
+        targetSdk = Version.TARGET_SDK_VERSION
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = Version.JAVA_VERSION
+        targetCompatibility = Version.JAVA_VERSION
+    }
+    kotlinOptions {
+        jvmTarget = Version.JAVA_VERSION.toString()
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Version.COMPOSE
+    }
+}
+
+dependencies {
+    implementation(Dependency.Compose.Activity)
+    implementation(Dependency.Compose.UI)
+    implementation(Dependency.Compose.PREVIEW)
+    implementation(Dependency.Compose.MATERIAL)
+    implementation(Dependency.Compose.COMPOSE_HILT_NAV)
+}
