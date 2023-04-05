82dc8997dabcf36dab42d0b07e71e8cf71da84e9
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-04-05 18:27:13 +0900

    기본 Shape 세팅

--- design-system/src/main/java/com/danbam/design_system/attribute/Shape.kt
+++ design-system/src/main/java/com/danbam/design_system/attribute/Shape.kt
@@ -1,11 +1,25 @@
 package com.danbam.design_system.attribute
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Shapes
+import androidx.compose.foundation.shape.ZeroCornerSize
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.unit.dp
 
-val Shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
+@Immutable
+data class IndiStrawShape(
+    val smallRoundedShape: RoundedCornerShape,
+    val bigRoundedShape: RoundedCornerShape,
+)
+
+internal val LocalIndiStrawShapes = staticCompositionLocalOf {
+    IndiStrawShape(
+        smallRoundedShape = RoundedCornerShape(ZeroCornerSize),
+        bigRoundedShape = RoundedCornerShape(ZeroCornerSize)
+    )
+}
+
+val defaultSahpe = IndiStrawShape(
+    smallRoundedShape = RoundedCornerShape(10.dp),
+    bigRoundedShape = RoundedCornerShape(20.dp)
 )
