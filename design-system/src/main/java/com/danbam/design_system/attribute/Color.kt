74d2ebc6b4fb3b4ef2bd8e043daac93363152c36
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-05-23 10:06:54 +0900

    기본 컬러 세팅

--- design-system/src/main/java/com/danbam/design_system/attribute/Color.kt
+++ design-system/src/main/java/com/danbam/design_system/attribute/Color.kt
@@ -1,8 +1,56 @@
 package com.danbam.design_system.attribute
 
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
+import javax.annotation.concurrent.Immutable
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+@Immutable
+data class IndiStrawColor(
+    val button: Color,
+    val background: Color,
+    val more: Color,
+    val text: Color,
+    val profile: Color,
+    val textBox: Color,
+    val exampleText: Color,
+    val error: Color,
+    val check: Color,
+)
+
+internal val LocalIndiStrawColors = staticCompositionLocalOf {
+    IndiStrawColor(
+        button = Color.Unspecified,
+        background = Color.Unspecified,
+        more = Color.Unspecified,
+        text = Color.Unspecified,
+        profile = Color.Unspecified,
+        textBox = Color.Unspecified,
+        exampleText = Color.Unspecified,
+        error = Color.Unspecified,
+        check = Color.Unspecified
+    )
+}
+
+val lightColor = IndiStrawColor(
+    button = Color(0xFFEB5959),
+    background = Color(0xFF000000),
+    more = Color(0xFF81B7F8),
+    text = Color(0xFFFFFFFF),
+    profile = Color(0xFFCC7979),
+    textBox = Color(0xFF4A4A4A),
+    exampleText = Color(0xFFB5B5B5),
+    error = Color(0xFFFF1717),
+    check = Color(0xFF60EE5D)
+)
+
+val darkColor = IndiStrawColor(
+    button = Color(0xFFEB5959),
+    background = Color(0xFF000000),
+    more = Color(0xFF81B7F8),
+    text = Color(0xFFFFFFFF),
+    profile = Color(0xFFCC7979),
+    textBox = Color(0xFF4A4A4A),
+    exampleText = Color(0xFFB5B5B5),
+    error = Color(0xFFFF1717),
+    check = Color(0xFF60EE5D)
+)
