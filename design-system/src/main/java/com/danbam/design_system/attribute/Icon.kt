551b59b88066f41b1c8c66dd0c3d28824d255ef7
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-05-25 11:51:53 +0900

    icon 생성하는 함수 구현

--- design-system/src/main/java/com/danbam/design_system/attribute/Icon.kt
+++ design-system/src/main/java/com/danbam/design_system/attribute/Icon.kt
@@ -1,90 +1,118 @@
 package com.danbam.design_system.attribute
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.DefaultAlpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import com.danbam.design_system.R
 import javax.annotation.concurrent.Immutable
 
+@Composable
+fun IndiStrawIcon(
+    modifier: Modifier = Modifier,
+    icon: IndiStrawIconList,
+    alignment: Alignment = Alignment.Center,
+    contentScale: ContentScale = ContentScale.Fit,
+    alpha: Float = DefaultAlpha,
+    colorFilter: ColorFilter? = null,
+) {
+    Image(
+        modifier = modifier,
+        painter = painterResource(id = icon.drawableId),
+        contentDescription = icon.contentDescription,
+        alignment = alignment,
+        contentScale = contentScale,
+        alpha = alpha,
+        colorFilter = colorFilter
+    )
+}
+
 @Immutable
-class IndiStrawIcon private constructor(
+class IndiStrawIconList private constructor(
     @DrawableRes val drawableId: Int,
     val contentDescription: String? = null,
 ) {
     companion object {
         @Stable
-        val OpenEyes = IndiStrawIcon(
+        val OpenEyes = IndiStrawIconList(
             drawableId = R.drawable.ic_open_eyes,
             contentDescription = "open eyes"
         )
 
         @Stable
-        val CloseEyes = IndiStrawIcon(
+        val CloseEyes = IndiStrawIconList(
             drawableId = R.drawable.ic_close_eyes,
             contentDescription = "close eyes"
         )
 
         @Stable
-        val Back = IndiStrawIcon(
+        val Back = IndiStrawIconList(
             drawableId = R.drawable.ic_back,
             contentDescription = "back"
         )
 
         @Stable
-        val Down = IndiStrawIcon(
+        val Down = IndiStrawIconList(
             drawableId = R.drawable.ic_down,
             contentDescription = "down"
         )
 
         @Stable
-        val DateArrow = IndiStrawIcon(
+        val DateArrow = IndiStrawIconList(
             drawableId = R.drawable.ic_date_arrow,
             contentDescription = "date arrow"
         )
 
         @Stable
-        val FastSearch = IndiStrawIcon(
+        val FastSearch = IndiStrawIconList(
             drawableId = R.drawable.ic_fast_search,
             contentDescription = "fast search"
         )
 
         @Stable
-        val Check = IndiStrawIcon(
+        val Check = IndiStrawIconList(
             drawableId = R.drawable.ic_check,
             contentDescription = "check"
         )
 
         @Stable
-        val UnCheck = IndiStrawIcon(
+        val UnCheck = IndiStrawIconList(
             drawableId = R.drawable.ic_uncheck,
             contentDescription = "un check"
         )
 
         @Stable
-        val Gallery = IndiStrawIcon(
+        val Gallery = IndiStrawIconList(
             drawableId = R.drawable.ic_gallery,
             contentDescription = "gallery"
         )
 
         @Stable
-        val Camera = IndiStrawIcon(
+        val Camera = IndiStrawIconList(
             drawableId = R.drawable.ic_camera,
             contentDescription = "camera"
         )
 
         @Stable
-        val Search = IndiStrawIcon(
+        val Search = IndiStrawIconList(
             drawableId = R.drawable.ic_search,
             contentDescription = "search"
         )
 
         @Stable
-        val Profile = IndiStrawIcon(
+        val Profile = IndiStrawIconList(
             drawableId = R.drawable.ic_profile,
             contentDescription = "profile"
         )
 
         @Stable
-        val NoImage = IndiStrawIcon(
+        val NoImage = IndiStrawIconList(
             drawableId = R.drawable.ic_no_img,
             contentDescription = "no image"
         )
