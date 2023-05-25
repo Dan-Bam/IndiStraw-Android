ad5d18c6bc0388169b40020ec1717180f2b2703f
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-05-25 23:58:54 +0900

    아이디 찾기, 비밀번호 찾기, 메인페이지 navItem 추가

--- presentation/src/main/java/com/danbam/presentation/util/NavigationUtil.kt
+++ presentation/src/main/java/com/danbam/presentation/util/NavigationUtil.kt
@@ -2,4 +2,10 @@ package com.danbam.presentation.util
 
 sealed class AppNavigationItem(val route: String) {
     object Login : AppNavigationItem("login")
+
+    object FindId : AppNavigationItem("findId")
+
+    object FindPassword : AppNavigationItem("findPassword")
+
+    object Main : AppNavigationItem("main")
 }
