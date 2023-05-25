2f941f16e55ad8eba85fb5879ac5bc679a4ada99
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-05-25 23:59:46 +0900

    아이디 찾기, 비밀번호 찾기, 메인페이지 route 구현

--- presentation/src/main/java/com/danbam/presentation/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/danbam/presentation/ui/main/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.danbam.design_system.IndiStrawTheme
+import com.danbam.presentation.ui.login.LoginScreen
 import com.danbam.presentation.util.AppNavigationItem
 
 class MainActivity : ComponentActivity() {
@@ -28,6 +29,16 @@ class MainActivity : ComponentActivity() {
 fun BaseApp(navController: NavHostController) {
     NavHost(navController = navController, startDestination = AppNavigationItem.Login.route) {
         composable(route = AppNavigationItem.Login.route) {
+            LoginScreen(navController = navController)
+        }
+        composable(route = AppNavigationItem.FindId.route) {
+
+        }
+        composable(route = AppNavigationItem.FindPassword.route) {
+
+        }
+        composable(route = AppNavigationItem.Main.route) {
+            
         }
     }
 }
