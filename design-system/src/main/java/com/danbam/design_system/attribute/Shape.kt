14df13bb956dc1e4db9aaf976a9693e3487cf9fa
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-05-25 09:00:28 +0900

    shape 추가

--- design-system/src/main/java/com/danbam/design_system/attribute/Shape.kt
+++ design-system/src/main/java/com/danbam/design_system/attribute/Shape.kt
@@ -12,6 +12,8 @@ data class IndiStrawShape(
     val defaultRounded: RoundedCornerShape,
     val bigRounded: RoundedCornerShape,
     val bottomSheet: RoundedCornerShape,
+    val topDefaultRounded: RoundedCornerShape,
+    val bottomDefaultRounded: RoundedCornerShape,
 )
 
 internal val LocalIndiStrawShapes = staticCompositionLocalOf {
@@ -20,12 +22,16 @@ internal val LocalIndiStrawShapes = staticCompositionLocalOf {
         defaultRounded = RoundedCornerShape(ZeroCornerSize),
         bigRounded = RoundedCornerShape(ZeroCornerSize),
         bottomSheet = RoundedCornerShape(ZeroCornerSize),
+        topDefaultRounded = RoundedCornerShape(ZeroCornerSize),
+        bottomDefaultRounded = RoundedCornerShape(ZeroCornerSize)
     )
 }
 
-val defaultSahpe = IndiStrawShape(
+val defaultShape = IndiStrawShape(
     smallRounded = RoundedCornerShape(5.dp),
     defaultRounded = RoundedCornerShape(10.dp),
     bigRounded = RoundedCornerShape(20.dp),
-    bottomSheet = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)
+    bottomSheet = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp),
+    topDefaultRounded = RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp),
+    bottomDefaultRounded = RoundedCornerShape(bottomStart = 10.dp, bottomEnd = 10.dp)
 )
