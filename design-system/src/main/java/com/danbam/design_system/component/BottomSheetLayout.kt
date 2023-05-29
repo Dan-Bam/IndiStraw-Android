cf7029c7ec5464616e7ef263c6763ab665a008f3
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-05-29 17:59:46 +0900

    bottomSheet 기본 shape 변경

--- design-system/src/main/java/com/danbam/design_system/component/BottomSheetLayout.kt
+++ design-system/src/main/java/com/danbam/design_system/component/BottomSheetLayout.kt
@@ -1,14 +1,28 @@
 package com.danbam.design_system.component
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Divider
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.danbam.design_system.IndiStrawTheme
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -17,10 +31,7 @@ fun IndiStrawBottomSheetLayout(
     sheetContent: @Composable ColumnScope.() -> Unit,
     content: @Composable (() -> Unit) -> Unit,
 ) {
-    val sheetState = rememberModalBottomSheetState(
-        initialValue = ModalBottomSheetValue.Hidden,
-        confirmStateChange = { it != ModalBottomSheetValue.HalfExpanded }
-    )
+    val sheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
     val coroutineScope = rememberCoroutineScope()
 
     BackHandler(sheetState.isVisible) {
@@ -28,9 +39,28 @@ fun IndiStrawBottomSheetLayout(
     }
 
     ModalBottomSheetLayout(
+        sheetContent = {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 13.dp)
+            ) {
+                Divider(
+                    modifier = Modifier
+                        .width(60.dp)
+                        .height(3.dp)
+                        .background(
+                            color = IndiStrawTheme.colors.exampleText,
+                            shape = IndiStrawTheme.shapes.smallRounded
+                        )
+                        .align(Alignment.CenterHorizontally)
+                )
+                sheetContent()
+            }
+        },
         sheetState = sheetState,
-        sheetContent = sheetContent,
-        sheetBackgroundColor = Color.Transparent
+        sheetShape = IndiStrawTheme.shapes.bottomSheet,
+        sheetBackgroundColor = IndiStrawTheme.colors.bottomSheet
     ) {
         content {
             coroutineScope.launch {
