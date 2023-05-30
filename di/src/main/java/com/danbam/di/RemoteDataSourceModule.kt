7ab75184e9e4143887249f590ac58e2dc5fd3aa5
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-05-30 13:38:51 +0900

    authAPI 세팅

--- di/src/main/java/com/danbam/di/RemoteDataSourceModule.kt
+++ di/src/main/java/com/danbam/di/RemoteDataSourceModule.kt
@@ -1,5 +1,8 @@
 package com.danbam.di
 
+import com.danbam.data.remote.datasource.AuthRemoteDataSource
+import com.danbam.data.remote.datasource.AuthRemoteDataSourceImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -7,5 +10,8 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RemoteDataSourceModule {
-
+    @Binds
+    abstract fun provideAuthRemoteDataSource(
+        authRemoteDataSourceImpl: AuthRemoteDataSourceImpl,
+    ): AuthRemoteDataSource
 }
