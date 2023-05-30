5a40616a0b938bf0a772f294ac0c40c83b3dbeec
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-05-30 14:13:22 +0900

    authPreference 세팅

--- di/src/main/java/com/danbam/di/LocalDataSourceModule.kt
+++ di/src/main/java/com/danbam/di/LocalDataSourceModule.kt
@@ -1,5 +1,8 @@
 package com.danbam.di
 
+import com.danbam.data.local.datasource.AuthLocalDataSource
+import com.danbam.data.local.datasource.AuthLocalDataSourceImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -7,5 +10,8 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class LocalDataSourceModule {
-
+    @Binds
+    abstract fun provideAuthLocalDataSource(
+        authLocalDataSourceImpl: AuthLocalDataSourceImpl,
+    ): AuthLocalDataSource
 }
--- di/src/main/java/com/danbam/di/PreferenceModule.kt
+++ di/src/main/java/com/danbam/di/PreferenceModule.kt
@@ -1,5 +1,8 @@
 package com.danbam.di
 
+import com.danbam.data.local.preference.AuthPreference
+import com.danbam.data.local.preference.AuthPreferenceImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -7,5 +10,8 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class PreferenceModule {
-
+    @Binds
+    abstract fun provideAuthPreference(
+        authPreferenceImpl: AuthPreferenceImpl,
+    ): AuthPreference
 }
