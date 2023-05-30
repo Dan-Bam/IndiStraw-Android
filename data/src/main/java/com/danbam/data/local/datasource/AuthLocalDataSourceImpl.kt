d77ee679663b985f5a1f50a5fbe2fe81a2499211
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-05-30 14:41:27 +0900

    authPreference 구현

---
+++ data/src/main/java/com/danbam/data/local/datasource/AuthLocalDataSourceImpl.kt
@@ -0,0 +1,44 @@
+package com.danbam.data.local.datasource
+
+import com.danbam.data.local.preference.AuthPreference
+import javax.inject.Inject
+
+class AuthLocalDataSourceImpl @Inject constructor(
+    private val authPreference: AuthPreference,
+) : AuthLocalDataSource {
+    override suspend fun saveAccessToken(accessToken: String) =
+        authPreference.saveAccessToken(accessToken = accessToken)
+
+    override suspend fun fetchAccessToken(): String =
+        authPreference.fetchAccessToken()
+
+    override suspend fun clearAccessToken() =
+        authPreference.clearAccessToken()
+
+    override suspend fun saveRefreshToken(refreshToken: String) =
+        authPreference.saveRefreshToken(refreshToken = refreshToken)
+
+    override suspend fun fetchRefreshToken(): String =
+        authPreference.fetchRefreshToken()
+
+    override suspend fun clearRefreshToken() =
+        authPreference.clearRefreshToken()
+
+    override suspend fun saveAccessExpiredAt(accessExpiredAt: String) =
+        authPreference.saveAccessExpiredAt(accessExpiredAt = accessExpiredAt)
+
+    override suspend fun fetchAccessExpiredAt(): String =
+        authPreference.fetchAccessExpiredAt()
+
+    override suspend fun clearAccessExpiredAt() =
+        authPreference.clearAccessExpiredAt()
+
+    override suspend fun saveRefreshExpiredAt(refreshExpiredAt: String) =
+        authPreference.saveRefreshExpiredAt(refreshExpiredAt = refreshExpiredAt)
+
+    override suspend fun fetchRefreshExpiredAt(): String =
+        authPreference.fetchRefreshExpiredAt()
+
+    override suspend fun clearRefreshExpiredAt() =
+        authPreference.clearRefreshExpiredAt()
+}
