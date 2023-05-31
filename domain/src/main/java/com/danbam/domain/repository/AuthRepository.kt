01891e289eb4390490715b66bbd7f5c3054ad38e
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-05-31 09:21:55 +0900

    login 성공 시 토큰 저장

--- domain/src/main/java/com/danbam/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/danbam/domain/repository/AuthRepository.kt
@@ -4,5 +4,5 @@ import com.danbam.domain.entity.LoginEntity
 import com.danbam.domain.param.LoginParam
 
 interface AuthRepository {
-    suspend fun login(loginParam: LoginParam): LoginEntity
+    suspend fun login(loginParam: LoginParam)
 }
