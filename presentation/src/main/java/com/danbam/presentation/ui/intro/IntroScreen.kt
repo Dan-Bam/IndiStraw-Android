8c8d22a3e6b926c2765847917bbd0bf378d6f81c
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-06-02 09:19:13 +0900

    자동 로그인 기능 추가

--- presentation/src/main/java/com/danbam/presentation/ui/intro/IntroScreen.kt
+++ presentation/src/main/java/com/danbam/presentation/ui/intro/IntroScreen.kt
@@ -1,5 +1,8 @@
 package com.danbam.presentation.ui.intro
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,16 +11,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.danbam.design_system.IndiStrawTheme
+import com.danbam.design_system.component.HeadLineBold
 import com.danbam.design_system.component.IndiStrawButton
 import com.danbam.design_system.component.JoinBold
 import com.danbam.design_system.component.TitleRegular
@@ -25,20 +28,49 @@ import com.danbam.design_system.util.indiStrawClickable
 import com.danbam.presentation.R
 import com.danbam.presentation.util.AppNavigationItem
 import com.danbam.presentation.util.SignUpNavigationItem
+import com.danbam.presentation.util.observeWithLifecycle
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.delay
 
+@OptIn(InternalCoroutinesApi::class)
 @Composable
 fun IntroScreen(
     navController: NavController,
+    introViewModel: IntroViewModel = hiltViewModel(),
 ) {
-    var isLogin by remember { mutableStateOf(true) }
+    val container = introViewModel.container
+    val state = container.stateFlow.collectAsState().value
+    val sideEffect = container.sideEffectFlow
+
+    sideEffect.observeWithLifecycle {
+        when (it) {
+            is IntroSideEffect.SuccessLogin -> {
+                delay(1_000L)
+                navController.navigate(AppNavigationItem.Main.route)
+            }
+        }
+    }
+
+    LaunchedEffect(true) {
+        introViewModel.isLogin()
+    }
+
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        if (isLogin) {
+        HeadLineBold(
+            modifier = Modifier.align(Alignment.Center),
+            text = stringResource(id = R.string.app_name),
+            fontSize = 30
+        )
+        AnimatedVisibility(
+            visible = state.isNeedLogin,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 39.dp),
+            enter = fadeIn(animationSpec = tween(durationMillis = 1500, delayMillis = 300))
+        ) {
             Column(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(bottom = 39.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 IndiStrawButton(
