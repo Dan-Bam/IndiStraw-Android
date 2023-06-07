72b982fd65eb59c4effd577461e528d0fa302d00
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-06-07 16:37:26 +0900

    비밀번호 변경 구현 및 예외처리

--- presentation/src/main/java/com/danbam/presentation/ui/find/find_password/FindPasswordScreen.kt
+++ presentation/src/main/java/com/danbam/presentation/ui/find/find_password/FindPasswordScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -14,23 +15,54 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.danbam.design_system.IndiStrawTheme
 import com.danbam.design_system.component.HeadLineBold
 import com.danbam.design_system.component.IndiStrawButton
 import com.danbam.design_system.component.IndiStrawHeader
 import com.danbam.design_system.component.IndiStrawTextField
+import com.danbam.design_system.component.TitleRegular
 import com.danbam.presentation.R
+import com.danbam.presentation.ui.login.LoginSideEffect
 import com.danbam.presentation.util.AppNavigationItem
+import com.danbam.presentation.util.observeWithLifecycle
+import kotlinx.coroutines.InternalCoroutinesApi
 
+@OptIn(InternalCoroutinesApi::class)
 @Composable
 fun FindPasswordScreen(
     navController: NavController,
     findPasswordViewModel: FindPasswordViewModel = hiltViewModel(),
     phoneNumber: String,
 ) {
+    val container = findPasswordViewModel.container
+    val state = container.stateFlow.collectAsState().value
+    val sideEffect = container.sideEffectFlow
+
     var password by remember { mutableStateOf("") }
-    var passwordVisible by remember { mutableStateOf(false) }
+    var errorText by remember { mutableStateOf("") }
     var checkPassword by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) }
     var checkPasswordVisible by remember { mutableStateOf(false) }
+
+    val errorList = mapOf(
+        FindPasswordSideEffect.EmptyException to stringResource(id = R.string.wrong_change_password_empty),
+        FindPasswordSideEffect.DifferentException to stringResource(id = R.string.wrong_password_different),
+        FindPasswordSideEffect.LengthException to stringResource(id = R.string.wrong_password_length),
+        FindPasswordSideEffect.MatchException to stringResource(id = R.string.wrong_password_match),
+        FindPasswordSideEffect.FailChangeException to stringResource(id = R.string.wait)
+    )
+
+    sideEffect.observeWithLifecycle {
+        when (it) {
+            is FindPasswordSideEffect.EmptyException, FindPasswordSideEffect.DifferentException, FindPasswordSideEffect.LengthException, FindPasswordSideEffect.MatchException, FindPasswordSideEffect.FailChangeException -> {
+                errorText = errorList[it]!!
+            }
+
+            is FindPasswordSideEffect.SuccessChange -> {
+
+            }
+        }
+    }
 
     Column(
         modifier = Modifier.fillMaxSize()
@@ -47,7 +79,10 @@ fun FindPasswordScreen(
             modifier = Modifier.padding(top = 66.dp),
             hint = stringResource(id = R.string.password),
             value = password,
-            onValueChange = { password = it },
+            onValueChange = {
+                if (errorText.isNotEmpty()) errorText = ""
+                password = it
+            },
             imeAction = ImeAction.Next,
             isToggleVisible = passwordVisible,
             onToggleChange = { passwordVisible = !passwordVisible }
@@ -56,15 +91,28 @@ fun FindPasswordScreen(
             modifier = Modifier.padding(top = 20.dp),
             hint = stringResource(id = R.string.check_password),
             value = checkPassword,
-            onValueChange = { checkPassword = it },
+            onValueChange = {
+                if (errorText.isNotEmpty()) errorText = ""
+                checkPassword = it
+            },
             isToggleVisible = checkPasswordVisible,
             onToggleChange = { checkPasswordVisible = !checkPasswordVisible }
+        )
+        TitleRegular(
+            modifier = Modifier.padding(start = 32.dp, top = 7.dp),
+            text = errorText,
+            color = IndiStrawTheme.colors.error,
+            fontSize = 12
         )
         IndiStrawButton(
             modifier = Modifier.padding(top = 37.dp),
             text = stringResource(id = R.string.check)
         ) {
-            findPasswordViewModel.changePassword(phoneNumber = phoneNumber, password = password, checkPassword = checkPassword)
+            findPasswordViewModel.changePassword(
+                phoneNumber = phoneNumber,
+                password = password,
+                checkPassword = checkPassword
+            )
             navController.navigate(AppNavigationItem.Login.route) {
                 popUpTo(AppNavigationItem.Intro.route)
             }
--- presentation/src/main/java/com/danbam/presentation/ui/find/find_password/FindPasswordViewModel.kt
+++ presentation/src/main/java/com/danbam/presentation/ui/find/find_password/FindPasswordViewModel.kt
@@ -4,30 +4,41 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.danbam.domain.param.ChangePasswordParam
 import com.danbam.domain.usecase.account.ChangePasswordUseCase
+import com.danbam.presentation.util.errorHandling
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.postSideEffect
 import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
 
 @HiltViewModel
 class FindPasswordViewModel @Inject constructor(
     private val changePasswordUseCase: ChangePasswordUseCase,
-) : ContainerHost<Unit, Unit>, ViewModel() {
-    override val container = container<Unit, Unit>(Unit)
+) : ContainerHost<Unit, FindPasswordSideEffect>, ViewModel() {
+    override val container = container<Unit, FindPasswordSideEffect>(Unit)
 
     fun changePassword(phoneNumber: String, password: String, checkPassword: String) = intent {
-        viewModelScope.launch {
-            changePasswordUseCase(
-                changePasswordParam = ChangePasswordParam(
-                    phoneNumber = phoneNumber,
-                    password = password
-                )
-            ).onSuccess {
-
-            }.onFailure {
-
+        if (password.isEmpty() || checkPassword.isEmpty()) postSideEffect(FindPasswordSideEffect.EmptyException)
+        else if (password != checkPassword) postSideEffect(FindPasswordSideEffect.DifferentException)
+        else if (password.length !in (8..20)) postSideEffect(FindPasswordSideEffect.LengthException)
+        else if (!"^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#\$%^&*?~])[0-9a-zA-Z!@#\$%^&*?~]+\$".toRegex()
+                .matches(password)
+        ) {
+            postSideEffect(FindPasswordSideEffect.MatchException)
+        } else {
+            viewModelScope.launch {
+                changePasswordUseCase(
+                    changePasswordParam = ChangePasswordParam(
+                        phoneNumber = phoneNumber,
+                        password = password
+                    )
+                ).onSuccess {
+                    postSideEffect(FindPasswordSideEffect.SuccessChange)
+                }.onFailure {
+                    it.errorHandling(unknownAction = { postSideEffect(FindPasswordSideEffect.FailChangeException) })
+                }
             }
         }
     }
