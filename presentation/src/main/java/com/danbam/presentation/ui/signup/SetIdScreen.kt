0f8602357e62c60f813b7b8d14244a333bf92dc4
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-06-08 15:11:26 +0900

    아이디 입력 페이지 포커싱 처리

--- presentation/src/main/java/com/danbam/presentation/ui/signup/SetIdScreen.kt
+++ presentation/src/main/java/com/danbam/presentation/ui/signup/SetIdScreen.kt
@@ -9,7 +9,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -19,12 +24,15 @@ import com.danbam.design_system.component.IndiStrawButton
 import com.danbam.design_system.component.IndiStrawHeader
 import com.danbam.design_system.component.IndiStrawTextField
 import com.danbam.design_system.component.TitleRegular
+import com.danbam.design_system.util.indiStrawClickable
 import com.danbam.presentation.R
 import com.danbam.presentation.util.SignUpNavigationItem
 import com.danbam.presentation.util.observeWithLifecycle
+import com.danbam.presentation.util.popBackStack
+import com.danbam.presentation.util.requestFocus
 import kotlinx.coroutines.InternalCoroutinesApi
 
-@OptIn(InternalCoroutinesApi::class)
+@OptIn(InternalCoroutinesApi::class, ExperimentalComposeUiApi::class)
 @Composable
 fun SetIdScreen(
     navController: NavController,
@@ -36,6 +44,9 @@ fun SetIdScreen(
 
     var id by remember { mutableStateOf("") }
     var errorText by remember { mutableStateOf("") }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusManager = LocalFocusManager.current
+    val idFocusRequester = remember { FocusRequester() }
 
     val errorList = mapOf(
         SignUpSideEffect.EmptyIdException to stringResource(id = R.string.require_id),
@@ -46,10 +57,12 @@ fun SetIdScreen(
     sideEffect.observeWithLifecycle {
         when (it) {
             is SignUpSideEffect.EmptyIdException, SignUpSideEffect.MatchIdException, SignUpSideEffect.EnrollIdException -> {
+                idFocusRequester.requestFocus(keyboardController = keyboardController)
                 errorText = errorList[it]!!
             }
 
             is SignUpSideEffect.Next -> {
+                keyboardController?.hide()
                 navController.navigate(SignUpNavigationItem.SetPassword.route)
             }
 
@@ -60,10 +73,15 @@ fun SetIdScreen(
     }
 
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .indiStrawClickable {
+                focusManager.clearFocus()
+                keyboardController?.hide()
+            }
     ) {
         IndiStrawHeader(marginTop = 25, backStringId = R.string.back, pressBackBtn = {
-            navController.popBackStack()
+            navController.popBackStack(keyboardController = keyboardController)
         })
         HeadLineBold(
             modifier = Modifier
@@ -71,7 +89,9 @@ fun SetIdScreen(
             text = stringResource(id = R.string.require_id)
         )
         IndiStrawTextField(
-            modifier = Modifier.padding(top = 96.dp),
+            modifier = Modifier
+                .padding(top = 96.dp)
+                .focusRequester(focusRequester = idFocusRequester),
             hint = stringResource(id = R.string.id),
             value = id,
             onValueChange = {
