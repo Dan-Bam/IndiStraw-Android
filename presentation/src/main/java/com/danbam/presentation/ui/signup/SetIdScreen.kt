41b1d6f2e588a1191522dc91115561296736d0e5
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-06-08 12:07:23 +0900

    아이디 입력 후 저장 및 예외처리

--- presentation/src/main/java/com/danbam/presentation/ui/signup/SetIdScreen.kt
+++ presentation/src/main/java/com/danbam/presentation/ui/signup/SetIdScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -12,18 +13,52 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.danbam.design_system.IndiStrawTheme
 import com.danbam.design_system.component.HeadLineBold
 import com.danbam.design_system.component.IndiStrawButton
 import com.danbam.design_system.component.IndiStrawHeader
 import com.danbam.design_system.component.IndiStrawTextField
+import com.danbam.design_system.component.TitleRegular
 import com.danbam.presentation.R
 import com.danbam.presentation.util.SignUpNavigationItem
+import com.danbam.presentation.util.observeWithLifecycle
+import kotlinx.coroutines.InternalCoroutinesApi
 
+@OptIn(InternalCoroutinesApi::class)
 @Composable
 fun SetIdScreen(
     navController: NavController,
+    signUpViewModel: SignUpViewModel,
 ) {
+    val container = signUpViewModel.container
+    val state = container.stateFlow.collectAsState().value
+    val sideEffect = container.sideEffectFlow
+
     var id by remember { mutableStateOf("") }
+    var errorText by remember { mutableStateOf("") }
+
+    val errorList = mapOf(
+        SignUpSideEffect.EmptyIdException to stringResource(id = R.string.require_id),
+        SignUpSideEffect.MatchIdException to stringResource(id = R.string.wrong_id_length),
+        SignUpSideEffect.EnrollIdException to stringResource(id = R.string.wrong_enroll_id)
+    )
+
+    sideEffect.observeWithLifecycle {
+        when (it) {
+            is SignUpSideEffect.EmptyIdException, SignUpSideEffect.MatchIdException, SignUpSideEffect.EnrollIdException -> {
+                errorText = errorList[it]!!
+            }
+
+            is SignUpSideEffect.Next -> {
+                navController.navigate(SignUpNavigationItem.SetPassword.route)
+            }
+
+            else -> {
+
+            }
+        }
+    }
+
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -39,12 +74,21 @@ fun SetIdScreen(
             modifier = Modifier.padding(top = 96.dp),
             hint = stringResource(id = R.string.id),
             value = id,
-            onValueChange = { id = it })
+            onValueChange = {
+                if (errorText.isNotEmpty()) errorText = ""
+                id = it
+            })
+        TitleRegular(
+            modifier = Modifier.padding(start = 32.dp, top = 7.dp),
+            text = errorText,
+            color = IndiStrawTheme.colors.error,
+            fontSize = 12
+        )
         IndiStrawButton(
             modifier = Modifier.padding(top = 78.dp),
             text = stringResource(id = R.string.next)
         ) {
-            navController.navigate(SignUpNavigationItem.SetPassword.route)
+            signUpViewModel.setId(id = id)
         }
     }
 }
--- presentation/src/main/java/com/danbam/presentation/ui/signup/SignUpViewModel.kt
+++ presentation/src/main/java/com/danbam/presentation/ui/signup/SignUpViewModel.kt
@@ -1,7 +1,11 @@
 package com.danbam.presentation.ui.signup
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.danbam.domain.usecase.auth.CheckIdUseCase
+import com.danbam.presentation.util.errorHandling
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
 import org.orbitmvi.orbit.syntax.simple.postSideEffect
@@ -11,6 +15,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SignUpViewModel @Inject constructor(
+    private val checkIdUseCase: CheckIdUseCase,
 ) : ContainerHost<SignUpState, SignUpSideEffect>, ViewModel() {
     override val container = container<SignUpState, SignUpSideEffect>(SignUpState())
 
@@ -19,6 +24,26 @@ class SignUpViewModel @Inject constructor(
         else {
             reduce { state.copy(name = name) }
             postSideEffect(SignUpSideEffect.Next)
+        }
+    }
+
+    fun setPhoneNumber(phoneNumber: String) = intent {
+        reduce { state.copy(phoneNumber = phoneNumber) }
+    }
+
+    fun setId(id: String) = intent {
+        if (id.isEmpty()) postSideEffect(SignUpSideEffect.EmptyIdException)
+        else if (id.length !in (6..15)) postSideEffect(SignUpSideEffect.MatchIdException)
+        else {
+            viewModelScope.launch {
+                checkIdUseCase(id = id).onFailure {
+                    it.errorHandling(unknownAction = {}, conflictException = {
+                        postSideEffect(SignUpSideEffect.EnrollIdException)
+                    }, noContentException = {
+                        postSideEffect(SignUpSideEffect.Next)
+                    })
+                }
+            }
         }
     }
 }
