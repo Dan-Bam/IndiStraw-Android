649c0abcf2f3a9da18f16621905b04145cf18fb3
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-06-12 10:07:00 +0900

    mainScreen navigation 연결

--- presentation/src/main/java/com/danbam/presentation/app/BaseActivity.kt
+++ presentation/src/main/java/com/danbam/presentation/app/BaseActivity.kt
@@ -20,6 +20,7 @@ import com.danbam.presentation.ui.find.find_id.FindIdScreen
 import com.danbam.presentation.ui.find.find_password.FindPasswordScreen
 import com.danbam.presentation.ui.intro.IntroScreen
 import com.danbam.presentation.ui.login.LoginScreen
+import com.danbam.presentation.ui.main.MainScreen
 import com.danbam.presentation.ui.profile.ProfileScreen
 import com.danbam.presentation.ui.signup.SetIdScreen
 import com.danbam.presentation.ui.signup.SetNameScreen
@@ -124,6 +125,7 @@ fun NavGraphBuilder.mainGraph(navController: NavHostController) {
         FindPasswordScreen(navController = navController, phoneNumber = phoneNumber)
     }
     composable(route = AppNavigationItem.Main.route) {
+        MainScreen(navController = navController)
     }
     composable(route = AppNavigationItem.Profile.route) {
         ProfileScreen(navController = navController)
