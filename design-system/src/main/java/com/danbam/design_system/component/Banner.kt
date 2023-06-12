6373ae71986ef7273a2a6cf571f0ccc9a4f11ee0
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-06-12 10:06:21 +0900

    banner 컴포넌트 구현

---
+++ design-system/src/main/java/com/danbam/design_system/component/Banner.kt
@@ -0,0 +1,99 @@
+package com.danbam.design_system.component
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.danbam.design_system.IndiStrawTheme
+import com.danbam.design_system.util.RemoveOverScrollLazyRow
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerScope
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+
+const val BannerTime = 3_000L
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun IndiStrawBanner(
+    modifier: Modifier = Modifier,
+    itemCount: Int,
+    content: @Composable PagerScope.(page: Int) -> Unit,
+) {
+    val state = rememberPagerState()
+
+    LaunchedEffect(state.currentPage) {
+        delay(BannerTime)
+        withContext(NonCancellable) {
+            state.animateScrollToPage(page = if (state.currentPage < itemCount - 1) state.currentPage + 1 else 0)
+        }
+    }
+
+    Column(
+        modifier = modifier
+            .padding(start = 15.dp, end = 15.dp, top = 21.dp)
+            .fillMaxWidth()
+    ) {
+        HorizontalPager(
+            modifier = Modifier
+                .background(Color.White)
+                .fillMaxHeight(0.26F),
+            count = itemCount,
+            state = state
+        ) {
+            content(it % itemCount)
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        RemoveOverScrollLazyRow(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            items(itemCount) {
+                Box {
+                    if (it == state.currentPage) {
+                        Box(
+                            modifier = Modifier
+                                .align(Alignment.Center)
+                                .width(9.dp)
+                                .height(9.dp)
+                                .background(
+                                    color = IndiStrawTheme.colors.text,
+                                    shape = IndiStrawTheme.shapes.circle
+                                )
+                        )
+                    }
+                    Box(
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .padding(horizontal = 5.dp)
+                            .width(5.dp)
+                            .height(5.dp)
+                            .background(
+                                color = if (it == state.currentPage) IndiStrawTheme.colors.main else IndiStrawTheme.colors.text,
+                                shape = IndiStrawTheme.shapes.circle
+                            )
+                    )
+                }
+            }
+        }
+    }
+}
