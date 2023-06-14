4e595facb192cb0b15c9eb5a7510bb2634b16a51
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-06-15 08:14:41 +0900

    프로필 페이지 리팩토링

--- presentation/src/main/java/com/danbam/presentation/ui/profile/ProfileScreen.kt
+++ presentation/src/main/java/com/danbam/presentation/ui/profile/ProfileScreen.kt
@@ -10,53 +10,45 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import com.danbam.design_system.IndiStrawTheme
 import com.danbam.design_system.attribute.IndiStrawIcon
 import com.danbam.design_system.attribute.IndiStrawIconList
-import com.danbam.design_system.component.FundingChip
 import com.danbam.design_system.component.HeadLineBold
 import com.danbam.design_system.component.IndiStrawColumnBackground
 import com.danbam.design_system.component.IndiStrawHeader
 import com.danbam.design_system.component.IndiStrawTab
 import com.danbam.design_system.component.IndiStrawTabRow
-import com.danbam.design_system.component.MovieChip
-import com.danbam.design_system.component.TitleSemiBold
 import com.danbam.design_system.R
-import com.danbam.presentation.util.parser.toDp
+import com.danbam.design_system.component.FundingTab
+import com.danbam.design_system.component.MovieTab
 
 @Composable
 fun ProfileScreen(
     navController: NavController,
 ) {
-    val context = LocalContext.current
-    var currentMovieChip: MovieChip by remember { mutableStateOf(MovieChip.RecentMovie) }
-    var currentFundingChip: FundingChip by remember { mutableStateOf(FundingChip.ParticipantFunding) }
-    var currentMovieIndex by remember { mutableStateOf(0) }
-    var currentFundingIndex by remember { mutableStateOf(0) }
-    val movieTabWidthList = remember { mutableStateListOf(0, 0) }
-    val fundingTabWidthList = remember { mutableStateListOf(0, 0) }
+    var currentMovieTab: MovieTab by remember { mutableStateOf(MovieTab.RecentMovie) }
+    var currentFundingTab: FundingTab by remember { mutableStateOf(FundingTab.ParticipantFunding) }
+    val scrollState = rememberScrollState()
 
-    IndiStrawColumnBackground {
+    IndiStrawColumnBackground(
+        modifier = Modifier.verticalScroll(scrollState)
+    ) {
         IndiStrawHeader(
             pressBackBtn = { navController.popBackStack() }) {
             Row {
@@ -84,7 +76,7 @@ fun ProfileScreen(
                 Box(
                     modifier = Modifier
                         .background(
-                            color = IndiStrawTheme.colors.exampleText,
+                            color = IndiStrawTheme.colors.gray,
                             shape = IndiStrawTheme.shapes.circle,
                         )
                         .padding(paddingValues = PaddingValues(23.dp)),
@@ -111,59 +103,43 @@ fun ProfileScreen(
             modifier = Modifier
                 .padding(top = 32.dp, start = 15.dp),
             itemList = listOf(""),
-            tabWidthList = movieTabWidthList,
-            tabHeader = {
-                IndiStrawTab(
-                    modifier = Modifier.onSizeChanged {
-                        movieTabWidthList[0] = it.width.toDp(context)
-                    },
-                    text = stringResource(id = R.string.recent_watch_movie),
-                    isSelect = currentMovieChip == MovieChip.RecentMovie
-                ) {
-                    currentMovieIndex = 0
-                    currentMovieChip = MovieChip.RecentMovie
-                }
-                Spacer(modifier = Modifier.width(12.dp))
-                IndiStrawTab(
-                    modifier = Modifier.onSizeChanged {
-                        movieTabWidthList[1] = it.width.toDp(context)
-                    },
-                    text = stringResource(id = R.string.participate_movie),
-                    isSelect = currentMovieChip == MovieChip.ParticipantMovie
-                ) {
-                    currentMovieIndex = 1
-                    currentMovieChip = MovieChip.ParticipantMovie
-                }
-            }, moreData = { }, selectedIndex = currentMovieIndex
+            tabHeader = listOf(
+                {
+                    IndiStrawTab(
+                        text = stringResource(id = R.string.recent_watch_movie),
+                        isSelect = currentMovieTab == MovieTab.RecentMovie
+                    ) {
+                        currentMovieTab = MovieTab.RecentMovie
+                    }
+                }, {
+                    IndiStrawTab(
+                        text = stringResource(id = R.string.participate_movie),
+                        isSelect = currentMovieTab == MovieTab.ParticipantMovie
+                    ) {
+                        currentMovieTab = MovieTab.ParticipantMovie
+                    }
+                }), moreData = { }
         )
         IndiStrawTabRow(
             modifier = Modifier
                 .padding(top = 43.dp, start = 15.dp),
             itemList = listOf(""),
-            tabWidthList = fundingTabWidthList,
-            tabHeader = {
-                IndiStrawTab(
-                    modifier = Modifier.onSizeChanged {
-                        fundingTabWidthList[0] = it.width.toDp(context)
-                    },
-                    text = stringResource(id = R.string.participate_funding),
-                    isSelect = currentFundingChip == FundingChip.ParticipantFunding
-                ) {
-                    currentFundingIndex = 0
-                    currentFundingChip = FundingChip.ParticipantFunding
-                }
-                Spacer(modifier = Modifier.width(12.dp))
-                IndiStrawTab(
-                    modifier = Modifier.onSizeChanged {
-                        fundingTabWidthList[1] = it.width.toDp(context)
-                    },
-                    text = stringResource(id = R.string.my_funding),
-                    isSelect = currentFundingChip == FundingChip.MyFunding
-                ) {
-                    currentFundingIndex = 1
-                    currentFundingChip = FundingChip.MyFunding
-                }
-            }, moreData = { }, isCrowdFunding = true, selectedIndex = currentFundingIndex
+            tabHeader = listOf(
+                {
+                    IndiStrawTab(
+                        text = stringResource(id = R.string.participate_funding),
+                        isSelect = currentFundingTab == FundingTab.ParticipantFunding
+                    ) {
+                        currentFundingTab = FundingTab.ParticipantFunding
+                    }
+                }, {
+                    IndiStrawTab(
+                        text = stringResource(id = R.string.my_funding),
+                        isSelect = currentFundingTab == FundingTab.MyFunding
+                    ) {
+                        currentFundingTab = FundingTab.MyFunding
+                    }
+                }), moreData = { }, isCrowdFunding = true
         )
     }
 }
