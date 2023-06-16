2c62252f8fc9d439258cdb1fb9c29296c7873324
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-06-16 13:52:10 +0900

    navigation 파일 분리 후 리패키징

--- presentation/src/main/java/com/danbam/presentation/app/BaseActivity.kt
+++ presentation/src/main/java/com/danbam/presentation/app/BaseActivity.kt
@@ -10,32 +10,16 @@ import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
-import androidx.navigation.navArgument
 import com.danbam.design_system.IndiStrawTheme
-import com.danbam.presentation.ui.certificate.CertificateScreen
-import com.danbam.presentation.ui.find.find_id.FindIdScreen
-import com.danbam.presentation.ui.find.find_password.FindPasswordScreen
-import com.danbam.presentation.ui.intro.IntroScreen
-import com.danbam.presentation.ui.login.LoginScreen
-import com.danbam.presentation.ui.main.MainScreen
-import com.danbam.presentation.ui.movie.detail.MovieDetailScreen
-import com.danbam.presentation.ui.movie.play.MoviePlayScreen
-import com.danbam.presentation.ui.profile.ProfileScreen
-import com.danbam.presentation.ui.signup.SetIdScreen
-import com.danbam.presentation.ui.signup.SetNameScreen
-import com.danbam.presentation.ui.signup.SetPasswordScreen
-import com.danbam.presentation.ui.signup.SetProfileScreen
+import com.danbam.presentation.navigation.MainNavigationItem
+import com.danbam.presentation.navigation.authGraph
+import com.danbam.presentation.navigation.mainGraph
+import com.danbam.presentation.navigation.movieGraph
+import com.danbam.presentation.navigation.profileGraph
+import com.danbam.presentation.navigation.signUpGraph
 import com.danbam.presentation.ui.signup.SignUpViewModel
-import com.danbam.presentation.util.view.AppNavigationItem
-import com.danbam.presentation.util.view.CertificateType
-import com.danbam.presentation.util.view.DeepLinkKey
-import com.danbam.presentation.util.view.MovieNavigationItem
-import com.danbam.presentation.util.view.SignUpNavigationItem
 import com.google.accompanist.navigation.animation.AnimatedNavHost
-import com.google.accompanist.navigation.animation.composable
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -59,7 +43,7 @@ fun BaseApp(navController: NavHostController) {
     val signUpViewModel: SignUpViewModel = hiltViewModel()
     AnimatedNavHost(
         navController = navController,
-        startDestination = AppNavigationItem.Intro.route,
+        startDestination = MainNavigationItem.Intro.route,
         enterTransition = {
             slideInHorizontally(
                 initialOffsetX = { it * 2 }, animationSpec = tween(
@@ -77,107 +61,9 @@ fun BaseApp(navController: NavHostController) {
         }
     ) {
         mainGraph(navController = navController)
+        authGraph(navController = navController)
         signUpGraph(navController = navController, signUpViewModel = signUpViewModel)
         movieGraph(navController = navController)
-    }
-}
-
-@OptIn(ExperimentalAnimationApi::class)
-fun NavGraphBuilder.mainGraph(navController: NavHostController) {
-    composable(route = AppNavigationItem.Intro.route) {
-        IntroScreen(navController = navController)
-    }
-    composable(route = AppNavigationItem.Login.route) {
-        LoginScreen(navController = navController)
-    }
-    composable(
-        route = AppNavigationItem.Certificate.route
-                + DeepLinkKey.CERTIFICATE_TYPE + "{${DeepLinkKey.CERTIFICATE_TYPE}}",
-        arguments = listOf(
-            navArgument(DeepLinkKey.CERTIFICATE_TYPE) {
-                type = NavType.StringType
-            }
-        )
-    ) {
-        val certificateType =
-            it.arguments?.getString(DeepLinkKey.CERTIFICATE_TYPE) ?: CertificateType.SIGN_UP
-
-        CertificateScreen(navController = navController, certificateType = certificateType)
-    }
-    composable(
-        route = AppNavigationItem.FindId.route
-                + DeepLinkKey.PHONE_NUMBER + "{${DeepLinkKey.PHONE_NUMBER}}",
-        arguments = listOf(
-            navArgument(DeepLinkKey.PHONE_NUMBER) {
-                type = NavType.StringType
-            }
-        )
-    ) {
-        val phoneNumber = it.arguments?.getString(DeepLinkKey.PHONE_NUMBER) ?: ""
-        FindIdScreen(navController = navController, phoneNumber = phoneNumber)
-    }
-    composable(
-        route = AppNavigationItem.FindPassword.route
-                + DeepLinkKey.PHONE_NUMBER + "{${DeepLinkKey.PHONE_NUMBER}}",
-        arguments = listOf(
-            navArgument(DeepLinkKey.PHONE_NUMBER) {
-                type = NavType.StringType
-            }
-        )
-    ) {
-        val phoneNumber = it.arguments?.getString(DeepLinkKey.PHONE_NUMBER) ?: ""
-        FindPasswordScreen(navController = navController, phoneNumber = phoneNumber)
-    }
-    composable(route = AppNavigationItem.Main.route) {
-        MainScreen(navController = navController)
-    }
-    composable(route = AppNavigationItem.Profile.route) {
-        ProfileScreen(navController = navController)
-    }
-}
-
-@OptIn(ExperimentalAnimationApi::class)
-fun NavGraphBuilder.signUpGraph(
-    navController: NavHostController,
-    signUpViewModel: SignUpViewModel,
-) {
-    composable(
-        route = SignUpNavigationItem.SetName.route
-    ) {
-        SetNameScreen(navController = navController, signUpViewModel = signUpViewModel)
-    }
-    composable(
-        route = SignUpNavigationItem.SetProfile.route
-                + DeepLinkKey.PHONE_NUMBER + "{${DeepLinkKey.PHONE_NUMBER}}",
-        arguments = listOf(
-            navArgument(DeepLinkKey.PHONE_NUMBER) {
-                type = NavType.StringType
-            }
-        )) {
-        val phoneNumber = it.arguments?.getString(DeepLinkKey.PHONE_NUMBER) ?: ""
-        SetProfileScreen(
-            navController = navController,
-            signUpViewModel = signUpViewModel,
-            phoneNumber = phoneNumber
-        )
-    }
-    composable(route = SignUpNavigationItem.SetId.route) {
-        SetIdScreen(navController = navController, signUpViewModel = signUpViewModel)
-    }
-    composable(route = SignUpNavigationItem.SetPassword.route) {
-        SetPasswordScreen(navController = navController, signUpViewModel = signUpViewModel)
-    }
-}
-
-@OptIn(ExperimentalAnimationApi::class)
-fun NavGraphBuilder.movieGraph(
-    navController: NavHostController,
-) {
-
-    composable(route = MovieNavigationItem.MovieDetail.route) {
-        MovieDetailScreen(navController = navController)
-    }
-    composable(route = MovieNavigationItem.MoviePlay.route) {
-        MoviePlayScreen()
+        profileGraph(navController = navController)
     }
 }
--- presentation/src/main/java/com/danbam/presentation/util/view/NavigationUtil.kt
+++ presentation/src/main/java/com/danbam/presentation/util/view/NavigationUtil.kt
@@ -4,39 +4,6 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.navigation.NavController
 
-sealed class AppNavigationItem(val route: String) {
-    object Intro : AppNavigationItem("intro")
-    object Login : AppNavigationItem("login")
-    object Certificate : AppNavigationItem("certificate")
-    object FindId : AppNavigationItem("findId")
-    object FindPassword : AppNavigationItem("findPassword")
-    object Main : AppNavigationItem("main")
-    object Profile : AppNavigationItem("profile")
-}
-
-sealed class SignUpNavigationItem(val route: String) {
-    object SetName : SignUpNavigationItem("setName")
-    object SetProfile : SignUpNavigationItem("setProfile")
-    object SetId : SignUpNavigationItem("setId")
-    object SetPassword : SignUpNavigationItem("setPassword")
-}
-
-sealed class MovieNavigationItem(val route: String) {
-    object MovieDetail : MovieNavigationItem("movieDetail")
-    object MoviePlay : MovieNavigationItem("moviePlay")
-}
-
-object DeepLinkKey {
-    const val CERTIFICATE_TYPE = "certificateType"
-    const val PHONE_NUMBER = "phoneNumber"
-}
-
-object CertificateType {
-    const val SIGN_UP = "signUp"
-    const val FIND_ID = "findId"
-    const val FIND_PASSWORD = "findPassword"
-}
-
 @OptIn(ExperimentalComposeUiApi::class)
 fun NavController.popBackStack(keyboardController: SoftwareKeyboardController? = null) {
     keyboardController?.hide()
