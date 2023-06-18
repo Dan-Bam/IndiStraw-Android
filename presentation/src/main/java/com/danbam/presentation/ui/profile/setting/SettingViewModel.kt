ec9c200e9a97837b9596e78f41d61baa89796466
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-06-18 13:22:21 +0900

    로그아웃 기능 추가

---
+++ presentation/src/main/java/com/danbam/presentation/ui/profile/setting/SettingViewModel.kt
@@ -0,0 +1,29 @@
+package com.danbam.presentation.ui.profile.setting
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.danbam.domain.usecase.auth.LogoutUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.postSideEffect
+import org.orbitmvi.orbit.viewmodel.container
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingViewModel @Inject constructor(
+    private val logoutUseCase: LogoutUseCase,
+) : ContainerHost<Unit, SettingSideEffect>, ViewModel() {
+    override val container = container<Unit, SettingSideEffect>(Unit)
+
+    fun logout() = intent {
+        viewModelScope.launch {
+            logoutUseCase().onSuccess {
+                postSideEffect(SettingSideEffect.SuccessLogout)
+            }.onFailure {
+                println("로그아웃 실패")
+            }
+        }
+    }
+}
