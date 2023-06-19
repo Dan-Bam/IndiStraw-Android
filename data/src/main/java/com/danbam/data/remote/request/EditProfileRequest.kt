dbd9884cdfd8210bf57cde827188a9593c0456c3
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-06-20 08:57:35 +0900

    editProfileRequest 파라미터 수정

--- data/src/main/java/com/danbam/data/remote/request/EditProfileRequest.kt
+++ data/src/main/java/com/danbam/data/remote/request/EditProfileRequest.kt
@@ -6,32 +6,11 @@ import com.google.gson.annotations.SerializedName
 data class EditProfileRequest(
     @SerializedName("name")
     val name: String,
-    @SerializedName("phoneNumber")
-    val phoneNumber: String,
-    @SerializedName("address")
-    val address: AddressRequest?,
     @SerializedName("profileUrl")
     val profileUrl: String?,
-) {
-    data class AddressRequest(
-        @SerializedName("zipcode")
-        val zipcode: String,
-        @SerializedName("streetAddress")
-        val streetAddress: String,
-        @SerializedName("detailAddress")
-        val detailAddress: String,
-    )
-}
+)
 
 fun EditProfileParam.toRequest() = EditProfileRequest(
     name = name,
-    phoneNumber = phoneNumber,
-    address = address?.toRequest(),
     profileUrl = profileUrl
-)
-
-fun EditProfileParam.AddressParam.toRequest() = EditProfileRequest.AddressRequest(
-    zipcode = zipcode,
-    streetAddress = streetAddress,
-    detailAddress = detailAddress
 )
--- domain/src/main/java/com/danbam/domain/param/EditProfileParam.kt
+++ domain/src/main/java/com/danbam/domain/param/EditProfileParam.kt
@@ -2,13 +2,5 @@ package com.danbam.domain.param
 
 data class EditProfileParam(
     val name: String,
-    val phoneNumber: String,
-    val address: AddressParam?,
     val profileUrl: String?,
-) {
-    data class AddressParam(
-        val zipcode: String,
-        val streetAddress: String,
-        val detailAddress: String,
-    )
-}
+)
