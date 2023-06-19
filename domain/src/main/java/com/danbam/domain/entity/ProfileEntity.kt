f84c5926b05df11debde9438f86812d62edb451a
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-06-19 17:23:28 +0900

    프로필 조회 시 핸드폰 번호, 주소 조회

--- domain/src/main/java/com/danbam/domain/entity/ProfileEntity.kt
+++ domain/src/main/java/com/danbam/domain/entity/ProfileEntity.kt
@@ -3,5 +3,7 @@ package com.danbam.domain.entity
 data class ProfileEntity(
     val id: String,
     val name: String,
+    val phoneNumber: String,
+    val address: String?,
     val profileUrl: String?,
 )
