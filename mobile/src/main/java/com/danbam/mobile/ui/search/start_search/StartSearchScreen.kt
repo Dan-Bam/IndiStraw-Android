29e2765c1627a3678d22615de60006abb551c8ad
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-06-20 17:26:56 +0900

    검색 시작 화면 퍼블리싱

---
+++ mobile/src/main/java/com/danbam/mobile/ui/search/start_search/StartSearchScreen.kt
@@ -0,0 +1,57 @@
+package com.danbam.mobile.ui.search.start_search
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.danbam.design_system.IndiStrawTheme
+import com.danbam.design_system.component.IndiStrawColumnBackground
+import com.danbam.design_system.component.TitleSemiBold
+import com.danbam.design_system.R
+import com.danbam.design_system.component.ExampleTextMedium
+import com.danbam.design_system.component.IndiStrawChipList
+import com.danbam.design_system.util.RemoveOverScrollLazyColumn
+
+@Composable
+fun StartSearchScreen(
+
+) {
+    IndiStrawColumnBackground {
+        TitleSemiBold(
+            modifier = Modifier.padding(start = 25.dp, top = 24.dp),
+            text = stringResource(id = R.string.popular_search),
+            color = IndiStrawTheme.colors.gray
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        IndiStrawChipList(itemList = listOf("최신영화", "신규영화"))
+        TitleSemiBold(
+            modifier = Modifier.padding(start = 25.dp, 36.dp),
+            text = stringResource(id = R.string.recent_search),
+            color = IndiStrawTheme.colors.gray
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        RemoveOverScrollLazyColumn {
+            itemsIndexed(
+                listOf(
+                    "슬램덩크",
+                    "슬램덩크",
+                    "슬램덩크",
+                    "슬램덩크",
+                    "슬램덩크",
+                    "슬램덩크",
+                    "슬램덩크",
+                    "슬램덩크",
+                    "슬램덩크",
+                    "슬램덩크"
+                )
+            ) { _, item ->
+                ExampleTextMedium(modifier = Modifier.padding(start = 25.dp), text = item)
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
+    }
+}
