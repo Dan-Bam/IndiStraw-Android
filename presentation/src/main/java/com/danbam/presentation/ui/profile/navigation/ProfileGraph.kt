90935cbbcb72cc752a3cb2e735a775fcb8db45b3
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-06-20 09:57:56 +0900

    상세 주소 페이지 네비게이션 세팅

--- presentation/src/main/java/com/danbam/presentation/ui/profile/navigation/ProfileGraph.kt
+++ presentation/src/main/java/com/danbam/presentation/ui/profile/navigation/ProfileGraph.kt
@@ -3,6 +3,9 @@ package com.danbam.presentation.ui.profile.navigation
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import com.danbam.presentation.ui.profile.detail_address.DetailAddressScreen
 import com.danbam.presentation.ui.profile.edit_profile.EditProfileScreen
 import com.danbam.presentation.ui.profile.find_address.FindAddressScreen
 import com.danbam.presentation.ui.profile.profile.ProfileScreen
@@ -14,6 +17,12 @@ sealed class ProfileNavigationItem(val route: String) {
     object Setting : ProfileNavigationItem("setting")
     object EditProfile : ProfileNavigationItem("editProfile")
     object FindAddress : ProfileNavigationItem("findAddress")
+    object DetailAddress : ProfileNavigationItem("detailAddress")
+}
+
+object ProfileDeepLinkKey {
+    const val ADDRESS = "address"
+    const val ZIP_CODE = "zipCode"
 }
 
 @OptIn(ExperimentalAnimationApi::class)
@@ -31,5 +40,22 @@ fun NavGraphBuilder.profileGraph(
     }
     composable(route = ProfileNavigationItem.FindAddress.route) {
         FindAddressScreen(navController = navController)
+    }
+    composable(
+        route = ProfileNavigationItem.DetailAddress.route
+                + ProfileDeepLinkKey.ADDRESS + "{${ProfileDeepLinkKey.ADDRESS}}"
+                + ProfileDeepLinkKey.ZIP_CODE + "{${ProfileDeepLinkKey.ZIP_CODE}}",
+        arguments = listOf(
+            navArgument(ProfileDeepLinkKey.ADDRESS) {
+                type = NavType.StringType
+            },
+            navArgument(ProfileDeepLinkKey.ZIP_CODE) {
+                type = NavType.StringType
+            }
+        )
+    ) {
+        val address = it.arguments?.getString(ProfileDeepLinkKey.ADDRESS) ?: ""
+        val zipCode = it.arguments?.getString(ProfileDeepLinkKey.ZIP_CODE) ?: ""
+        DetailAddressScreen(navController = navController, address = address, zipCode = zipCode)
     }
 }
