97be86f1a4d9c20789bc29a1373720f5d66be3bf
윤지빈 <81063667+100Seung-Min@users.noreply.github.com>
2023-06-27 20:17:37 +0900

    EndPoint 파일로 관리

---
+++ data/src/main/java/com/danbam/data/remote/util/EndPoint.kt
@@ -0,0 +1,10 @@
+package com.danbam.data.remote.util
+
+class EndPoint {
+    companion object {
+        const val ACCOUNT = "account"
+        const val AUTH = "auth"
+        const val FILE = "file"
+        const val SEARCH = "search"
+    }
+}
