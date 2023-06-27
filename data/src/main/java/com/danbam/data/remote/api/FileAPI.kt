0710617f82866bf083d2b5de2afeda1f67cbfe4f
윤지빈 <81063667+100Seung-Min@users.noreply.github.com>
2023-06-27 20:18:58 +0900

    endPoint 적용

--- data/src/main/java/com/danbam/data/remote/api/FileAPI.kt
+++ data/src/main/java/com/danbam/data/remote/api/FileAPI.kt
@@ -1,6 +1,7 @@
 package com.danbam.data.remote.api
 
 import com.danbam.data.remote.response.FileResponse
+import com.danbam.data.remote.util.EndPoint
 import okhttp3.MultipartBody
 import retrofit2.http.Multipart
 import retrofit2.http.POST
@@ -8,7 +9,7 @@ import retrofit2.http.Part
 
 interface FileAPI {
     @Multipart
-    @POST("file/")
+    @POST("${EndPoint.FILE}/")
     suspend fun sendFile(
         @Part file: MultipartBody.Part,
     ): FileResponse
