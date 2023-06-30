9814859d53e1cf80316f74e8fdbf4647c62a0395
윤지빈 <81063667+100Seung-Min@users.noreply.github.com>
2023-06-30 16:15:20 +0900

    인기 크라우드 펀딩 조회 기능 구현

--- mobile/src/main/java/com/danbam/mobile/ui/main/main/MainContract.kt
+++ mobile/src/main/java/com/danbam/mobile/ui/main/main/MainContract.kt
@@ -1,5 +1,8 @@
 package com.danbam.mobile.ui.main.main
 
+import com.danbam.domain.entity.FundingEntity
+
 data class MainState(
     val profileUrl: String? = null,
+    val fundingPopularList: List<FundingEntity> = listOf()
 )
