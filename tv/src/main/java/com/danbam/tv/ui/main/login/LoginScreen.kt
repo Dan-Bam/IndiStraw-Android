f95fed709b5f1f4b86f945e66a7f8196df22d852
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-02 01:56:07 +0900

    로그인 기능 구현

--- tv/src/main/java/com/danbam/tv/ui/main/login/LoginScreen.kt
+++ tv/src/main/java/com/danbam/tv/ui/main/login/LoginScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -12,22 +13,65 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.danbam.design_system.component.HeadLineBold
 import com.danbam.design_system.R
 import com.danbam.design_system.component.ExampleTextRegular
 import com.danbam.design_system.component.IndiStrawTvBackground
 import com.danbam.design_system.component.IndiStrawTvButton
+import com.danbam.design_system.component.IndiStrawTvDialog
 import com.danbam.design_system.component.IndiStrawTvTextField
 import com.danbam.tv.ui.main.navigation.MainNavigationItem
+import com.danbam.tv.util.android.observeWithLifecycle
+import kotlinx.coroutines.InternalCoroutinesApi
 
+@OptIn(InternalCoroutinesApi::class)
 @Composable
 fun LoginScreen(
-    navController: NavController
+    navController: NavController,
+    loginViewModel: LoginViewModel = hiltViewModel()
 ) {
+    val container = loginViewModel.container
+    val state = container.stateFlow.collectAsState().value
+    val sideEffect = container.sideEffectFlow
+
     var id by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var errorText by remember { mutableStateOf("") }
+    var errorDialogVisible by remember { mutableStateOf(false) }
+
+    val errorList = mapOf(
+        LoginSideEffect.EmptyIdException to stringResource(id = R.string.require_id),
+        LoginSideEffect.EmptyPasswordException to stringResource(id = R.string.require_password),
+        LoginSideEffect.MatchIdException to stringResource(id = R.string.wrong_id),
+        LoginSideEffect.MatchPasswordException to stringResource(id = R.string.wrong_password)
+    )
+
+    sideEffect.observeWithLifecycle {
+        when (it) {
+            is LoginSideEffect.LoginSuccess -> {
+            }
+
+            is LoginSideEffect.EmptyIdException, LoginSideEffect.MatchIdException -> {
+                errorDialogVisible = true
+                errorText = errorList[it]!!
+            }
+
+            is LoginSideEffect.EmptyPasswordException, LoginSideEffect.MatchPasswordException -> {
+                errorDialogVisible = true
+                errorText = errorList[it]!!
+            }
+
+            is LoginSideEffect.UnKnownException -> {}
+        }
+    }
+
     IndiStrawTvBackground {
+        IndiStrawTvDialog(
+            visible = errorDialogVisible,
+            content = errorText,
+            onDismissRequest = { errorDialogVisible = false })
         Column(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
@@ -47,7 +91,7 @@ fun LoginScreen(
 
             Spacer(modifier = Modifier.fillMaxHeight(0.09F))
             IndiStrawTvButton(text = stringResource(id = R.string.login)) {
-
+                loginViewModel.login(id = id, password = password)
             }
             Spacer(modifier = Modifier.fillMaxHeight(0.02F))
             ExampleTextRegular(text = "OR", fontSize = 24)
