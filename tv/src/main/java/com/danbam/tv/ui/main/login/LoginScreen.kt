a76ac4e397b9792650c3e2d59e947984cad0ff2c
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-01 23:40:34 +0900

    tv 로그인 화면 퍼블리싱

--- tv/src/main/java/com/danbam/tv/ui/main/login/LoginScreen.kt
+++ tv/src/main/java/com/danbam/tv/ui/main/login/LoginScreen.kt
@@ -1,13 +1,9 @@
 package com.danbam.tv.ui.main.login
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,19 +11,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.danbam.design_system.IndiStrawTheme
 import com.danbam.design_system.component.HeadLineBold
 import com.danbam.design_system.R
-import com.danbam.design_system.component.ButtonMedium
-import com.danbam.design_system.component.ExampleTextMedium
-import com.danbam.design_system.component.IndiStrawRowBackground
+import com.danbam.design_system.component.ExampleTextRegular
+import com.danbam.design_system.component.IndiStrawTvBackground
 import com.danbam.design_system.component.IndiStrawTvButton
 import com.danbam.design_system.component.IndiStrawTvTextField
-import com.danbam.design_system.util.indiStrawClickable
 
 @Composable
 fun LoginScreen(
@@ -35,15 +26,14 @@ fun LoginScreen(
 ) {
     var id by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    var errorText by remember { mutableStateOf("") }
-    IndiStrawRowBackground {
+    IndiStrawTvBackground {
         Column(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Spacer(modifier = Modifier.fillMaxHeight(0.08F))
+            Spacer(modifier = Modifier.fillMaxHeight(0.1F))
             HeadLineBold(text = stringResource(id = R.string.app_name), fontSize = 57)
-            Spacer(modifier = Modifier.fillMaxHeight(0.08F))
+            Spacer(modifier = Modifier.fillMaxHeight(0.05F))
             IndiStrawTvTextField(
                 hint = stringResource(id = R.string.id),
                 value = id,
@@ -54,14 +44,14 @@ fun LoginScreen(
                 value = password,
                 onValueChange = { password = it })
 
-            Spacer(modifier = Modifier.fillMaxHeight(0.08F))
+            Spacer(modifier = Modifier.fillMaxHeight(0.09F))
             IndiStrawTvButton(text = stringResource(id = R.string.login)) {
 
             }
-            Spacer(modifier = Modifier.fillMaxHeight(0.04F))
-            ExampleTextMedium(text = "or", fontSize = 20)
-            Spacer(modifier = Modifier.fillMaxHeight(0.04F))
-            IndiStrawTvButton(text = "QR로 로그인") {
+            Spacer(modifier = Modifier.fillMaxHeight(0.02F))
+            ExampleTextRegular(text = "OR", fontSize = 24)
+            Spacer(modifier = Modifier.fillMaxHeight(0.02F))
+            IndiStrawTvButton(text = stringResource(id = R.string.qr_login)) {
 
             }
         }
