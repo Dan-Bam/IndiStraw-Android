b41cda360a32a11bc6bf69b0169c0e708aa4c918
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-02 00:18:56 +0900

    qrLogin graph 세팅

--- tv/src/main/java/com/danbam/tv/ui/main/navigation/MainGraph.kt
+++ tv/src/main/java/com/danbam/tv/ui/main/navigation/MainGraph.kt
@@ -4,15 +4,20 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import com.danbam.tv.ui.main.login.LoginScreen
+import com.danbam.tv.ui.main.qr_login.QRLoginScreen
 import com.google.accompanist.navigation.animation.composable
 
 sealed class MainNavigationItem(val route: String) {
     object Login : MainNavigationItem("login")
+    object QRLogin : MainNavigationItem("QRLogin")
 }
 
 @OptIn(ExperimentalAnimationApi::class)
 fun NavGraphBuilder.mainGraph(navController: NavHostController) {
     composable(route = MainNavigationItem.Login.route) {
         LoginScreen(navController = navController)
+    }
+    composable(route = MainNavigationItem.QRLogin.route) {
+        QRLoginScreen(navController = navController)
     }
 }
