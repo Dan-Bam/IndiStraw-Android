15a8da9a09a4b718485cc96eacf0b2d11c2a14ff
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-02 17:46:42 +0900

    findActivity 함수 추가

---
+++ tv/src/main/java/com/danbam/tv/util/android/ContextUtil.kt
@@ -0,0 +1,11 @@
+package com.danbam.tv.util.android
+
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+
+fun Context.findActivity(): Activity? = when (this) {
+    is Activity -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else -> null
+}
