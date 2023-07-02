d92e01e235b0ef9ebf77b50e524e803ce22e5240
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-02 17:05:40 +0900

    rewardItem 구현

--- design-system/src/main/java/com/danbam/design_system/component/Item.kt
+++ design-system/src/main/java/com/danbam/design_system/component/Item.kt
@@ -19,6 +19,8 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.danbam.design_system.IndiStrawTheme
 import com.danbam.design_system.util.indiStrawClickable
+import com.danbam.design_system.util.toMoney
+import com.danbam.domain.entity.FundingDetailEntity
 import com.danbam.domain.entity.FundingEntity
 
 sealed class MovieType {
@@ -136,6 +138,54 @@ fun MovieItem(
                     Spacer(modifier = Modifier.weight(1F))
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun RewardItem(
+    item: FundingDetailEntity.RewardEntity
+) {
+    val rewardImageSize = LocalConfiguration.current.screenWidthDp * 0.3
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 15.dp)
+            .background(
+                color = IndiStrawTheme.colors.darkGray,
+                shape = IndiStrawTheme.shapes.defaultRounded
+            )
+            .padding(12.dp)
+    ) {
+        AsyncImage(
+            modifier = Modifier
+                .size(rewardImageSize.dp)
+                .align(Alignment.CenterVertically)
+                .clip(IndiStrawTheme.shapes.smallRounded),
+            model = item.imageUrl,
+            contentDescription = "crowdFundingImg",
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.width(12.dp))
+        Column(
+            modifier = Modifier
+                .weight(1F)
+                .height(rewardImageSize.dp)
+        ) {
+            HeadLineBold(
+                text = item.title,
+                fontSize = 14,
+                maxLines = 1
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            TitleRegular(
+                text = item.description,
+                color = IndiStrawTheme.colors.gray,
+                maxLines = 2,
+                fontSize = 14
+            )
+            Spacer(modifier = Modifier.weight(1F))
+            TitleSemiBold(text = "${item.price.toMoney()}원")
         }
     }
 }
