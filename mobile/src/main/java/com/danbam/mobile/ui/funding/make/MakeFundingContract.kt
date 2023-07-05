00808a128bfd0c7a9b14218eacd5c085964a8e4c
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-05 22:39:37 +0900

    directorAccountParam, rewardList state 추가

--- mobile/src/main/java/com/danbam/mobile/ui/funding/make/MakeFundingContract.kt
+++ mobile/src/main/java/com/danbam/mobile/ui/funding/make/MakeFundingContract.kt
@@ -1,11 +1,17 @@
 package com.danbam.mobile.ui.funding.make
 
+import com.danbam.domain.param.FundingCreateParam
 import java.time.LocalDate
 
 data class MakeFundingState(
     val title: String = "",
     val description: String = "",
     val targetAmount: Long = 0,
+    val directorAccountParam: FundingCreateParam.DirectorAccountParam = FundingCreateParam.DirectorAccountParam(
+        "",
+        ""
+    ),
+    val rewardList: List<FundingCreateParam.RewardParam> = listOf(),
     val endDate: LocalDate = LocalDate.now(),
     val thumbnailUrl: String? = null,
     val imageList: List<String> = listOf(),
