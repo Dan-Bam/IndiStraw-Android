b250c2cff6213c6974e2b4a4598e8a7b6844c2fe
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-05 22:39:54 +0900

    리워드 추가 기능 구현

--- mobile/src/main/java/com/danbam/mobile/ui/funding/make/WriteRewardScreen.kt
+++ mobile/src/main/java/com/danbam/mobile/ui/funding/make/WriteRewardScreen.kt
@@ -1,13 +1,12 @@
 package com.danbam.mobile.ui.funding.make
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,58 +14,45 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.danbam.design_system.IndiStrawTheme
 import com.danbam.design_system.component.IndiStrawButton
 import com.danbam.design_system.component.IndiStrawColumnBackground
 import com.danbam.design_system.R
-import com.danbam.design_system.attribute.IndiStrawIcon
-import com.danbam.design_system.attribute.IndiStrawIconList
+import com.danbam.design_system.component.ExampleTextMedium
 import com.danbam.design_system.component.IndiStrawTextField
+import com.danbam.design_system.component.IndiStrawToggle
+import com.danbam.design_system.component.SelectImageButton
 import com.danbam.design_system.component.TitleRegular
+import com.danbam.mobile.util.parser.toFile
 
 @Composable
 fun WriteRewardScreen(
+    makeFundingViewModel: MakeFundingViewModel,
     onAdd: () -> Unit
 ) {
+    val context = LocalContext.current
     var title by remember { mutableStateOf("") }
-    var introduce by remember { mutableStateOf("") }
-    IndiStrawColumnBackground {
+    var description by remember { mutableStateOf("") }
+    var isReal by remember { mutableStateOf(false) }
+    var amount by remember { mutableStateOf("") }
+    var thumbnailUrl: String? by remember { mutableStateOf(null) }
+    IndiStrawColumnBackground(
+        scrollEnabled = true
+    ) {
         TitleRegular(
             modifier = Modifier.padding(start = 32.dp, top = 36.dp, bottom = 16.dp),
             text = stringResource(id = R.string.thumbnail)
         )
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 32.dp)
-                .background(
-                    color = IndiStrawTheme.colors.gray,
-                    shape = IndiStrawTheme.shapes.bigRounded
-                )
-                .padding(top = 36.dp, bottom = 29.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            TitleRegular(
-                text = stringResource(id = R.string.require_thumbnail),
-                color = IndiStrawTheme.colors.black,
-                fontSize = 14
-            )
-            Spacer(modifier = Modifier.height(29.dp))
-            Row(
-                modifier = Modifier
-                    .background(
-                        color = IndiStrawTheme.colors.main,
-                        shape = IndiStrawTheme.shapes.bigRounded
-                    )
-                    .padding(horizontal = 12.dp, vertical = 6.dp)
-            ) {
-                IndiStrawIcon(icon = IndiStrawIconList.Plus)
-                Spacer(modifier = Modifier.width(6.dp))
-                TitleRegular(text = stringResource(id = R.string.upload_image), fontSize = 14)
+        SelectImageButton(imageUrl = thumbnailUrl, selectGallery = {
+            it?.let {
+                makeFundingViewModel.uploadImage(it.toFile(context)) {
+                    thumbnailUrl = it
+                }
             }
-        }
+        })
         TitleRegular(
             modifier = Modifier.padding(start = 32.dp, top = 28.dp, bottom = 16.dp),
             text = stringResource(id = R.string.title)
@@ -77,14 +63,58 @@ fun WriteRewardScreen(
             onValueChange = { title = it })
         TitleRegular(
             modifier = Modifier.padding(start = 32.dp, top = 28.dp, bottom = 16.dp),
+            text = stringResource(id = R.string.money)
+        )
+        IndiStrawTextField(
+            hint = stringResource(id = R.string.require_money),
+            value = description,
+            onValueChange = { description = it })
+        TitleRegular(
+            modifier = Modifier.padding(start = 32.dp, top = 28.dp, bottom = 16.dp),
             text = stringResource(id = R.string.introduce)
         )
         IndiStrawTextField(
             hint = stringResource(id = R.string.require_introduce),
-            value = introduce,
-            onValueChange = { introduce = it })
+            value = description,
+            onValueChange = { description = it })
+        TitleRegular(
+            modifier = Modifier.padding(start = 32.dp, top = 28.dp, bottom = 16.dp),
+            text = stringResource(id = R.string.is_real)
+        )
+        Row(
+            modifier = Modifier
+                .padding(horizontal = 32.dp)
+                .fillMaxWidth()
+                .background(IndiStrawTheme.colors.darkGray, IndiStrawTheme.shapes.defaultRounded)
+                .padding(horizontal = 13.dp, 14.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            ExampleTextMedium(text = stringResource(id = R.string.is_real_reward))
+            IndiStrawToggle(isChecked = isReal, onChecked = { isReal = it })
+        }
+        if (isReal) {
+            TitleRegular(
+                modifier = Modifier.padding(start = 32.dp, top = 28.dp, bottom = 16.dp),
+                text = stringResource(id = R.string.amount)
+            )
+            IndiStrawTextField(
+                hint = stringResource(id = R.string.require_amount),
+                value = amount,
+                onValueChange = { amount = it })
+        }
+        Spacer(modifier = Modifier.height(37.dp))
         IndiStrawButton(text = stringResource(id = R.string.add)) {
+            makeFundingViewModel.addReward(
+                thumbnailUrl = thumbnailUrl,
+                title = title,
+                description = description,
+                isReal = isReal,
+                amount = amount.toLongOrNull(),
+                onAdded = onAdd
+            )
             onAdd()
         }
+        Spacer(modifier = Modifier.height(93.dp))
     }
 }
