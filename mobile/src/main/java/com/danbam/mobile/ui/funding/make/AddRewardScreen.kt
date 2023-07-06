e0585f4696af39f3df41e76f368f9d62d6626658
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-06 15:48:24 +0900

    리워드 추가 화면 기능 구현

--- mobile/src/main/java/com/danbam/mobile/ui/funding/make/AddRewardScreen.kt
+++ mobile/src/main/java/com/danbam/mobile/ui/funding/make/AddRewardScreen.kt
@@ -1,18 +1,16 @@
 package com.danbam.mobile.ui.funding.make
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -24,56 +22,64 @@ import com.danbam.design_system.R
 import com.danbam.design_system.attribute.IndiStrawIcon
 import com.danbam.design_system.attribute.IndiStrawIconList
 import com.danbam.design_system.component.HeadLineBold
+import com.danbam.design_system.component.RewardItem
 import com.danbam.design_system.component.TitleRegular
+import com.danbam.design_system.util.RemoveOverScrollLazyColumn
 import com.danbam.design_system.util.indiStrawClickable
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
+import com.danbam.domain.entity.FundingDetailEntity
 
 @Composable
 fun AddRewardScreen(
+    makeFundingViewModel: MakeFundingViewModel,
     onAdd: () -> Unit,
     onNext: () -> Unit
 ) {
-    val itemList = remember { mutableStateListOf<Int>() }
-    val scrollState = rememberScrollState()
+    val container = makeFundingViewModel.container
+    val state = container.stateFlow.collectAsState().value
+    val sideEffect = container.sideEffectFlow
+
     Spacer(modifier = Modifier.padding(top = 20.dp))
     IndiStrawColumnBackground {
         HeadLineBold(
             modifier = Modifier.padding(start = 32.dp, bottom = 28.dp),
             text = stringResource(id = R.string.add_reward)
         )
-        Column(
-            modifier = Modifier
-                .verticalScroll(scrollState)
-                .weight(1F)
+        RemoveOverScrollLazyColumn(
+            modifier = Modifier.weight(1F),
+            verticalArrangement = if (state.rewardList.isEmpty()) Arrangement.Center else Arrangement.Top
         ) {
-            repeat(itemList.size) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(60.dp)
-                        .background(IndiStrawTheme.colors.gray)
+            itemsIndexed(state.rewardList) { index, item ->
+                RewardItem(
+                    item = FundingDetailEntity.RewardEntity(
+                        title = item.title,
+                        description = item.description,
+                        price = item.price,
+                        imageUrl = item.imageUrl
+                    ),
+                    onDelete = {
+                        makeFundingViewModel.removeReward(index)
+                    }
                 )
                 Spacer(modifier = Modifier.height(23.dp))
             }
-            if (itemList.size == 0) Spacer(modifier = Modifier.weight(1F))
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Box(
+            item(1) {
+                Column(
                     modifier = Modifier
-                        .border(1.dp, IndiStrawTheme.colors.white, IndiStrawTheme.shapes.circle)
-                        .padding(10.dp)
-                        .indiStrawClickable(onClick = onAdd)
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    IndiStrawIcon(icon = IndiStrawIconList.Plus)
+                    Box(
+                        modifier = Modifier
+                            .border(1.dp, IndiStrawTheme.colors.white, IndiStrawTheme.shapes.circle)
+                            .padding(10.dp)
+                            .indiStrawClickable(onClick = onAdd)
+                    ) {
+                        IndiStrawIcon(icon = IndiStrawIconList.Plus)
+                    }
+                    Spacer(modifier = Modifier.height(12.dp))
+                    TitleRegular(text = stringResource(id = R.string.do_add_reward))
                 }
-                Spacer(modifier = Modifier.height(12.dp))
-                TitleRegular(text = stringResource(id = R.string.do_add_reward))
             }
-            Spacer(modifier = Modifier.weight(1F))
         }
         Spacer(modifier = Modifier.height(20.dp))
         IndiStrawButton(text = stringResource(id = R.string.next)) {
