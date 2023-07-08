57e78949bc2020e22afb64fa3c84524793a925ae
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-09 07:18:23 +0900

    독립 영화 만들기 이동 기능 구현

--- mobile/src/main/java/com/danbam/mobile/ui/main/main/MainScreen.kt
+++ mobile/src/main/java/com/danbam/mobile/ui/main/main/MainScreen.kt
@@ -182,7 +182,9 @@ fun MainScreen(
                 .padding(vertical = 18.dp),
         ) {
             ExampleTextMedium(
-                modifier = Modifier.padding(horizontal = 13.dp),
+                modifier = Modifier
+                    .padding(horizontal = 13.dp)
+                    .indiStrawClickable { navController.navigate(MovieNavigationItem.WriteIntroduce.route) },
                 text = stringResource(id = R.string.make_indi_movie)
             )
             Divider(
