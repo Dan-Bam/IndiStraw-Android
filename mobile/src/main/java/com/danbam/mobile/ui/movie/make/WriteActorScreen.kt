fc3866bf59a7854d28c097ba60b6b4af809f0753
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-09 07:04:22 +0900

    영화 제작 네비게이션 세팅

---
+++ mobile/src/main/java/com/danbam/mobile/ui/movie/make/WriteActorScreen.kt
@@ -0,0 +1,17 @@
+package com.danbam.mobile.ui.movie.make
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.danbam.design_system.component.IndiStrawColumnBackground
+import com.danbam.design_system.component.IndiStrawHeader
+
+@Composable
+fun WriteActorScreen(
+    navController: NavController
+) {
+    IndiStrawColumnBackground {
+        IndiStrawHeader(
+            pressBackBtn = { navController.popBackStack() }
+        )
+    }
+}
--- mobile/src/main/java/com/danbam/mobile/ui/movie/navigation/MovieGraph.kt
+++ mobile/src/main/java/com/danbam/mobile/ui/movie/navigation/MovieGraph.kt
@@ -5,6 +5,10 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import com.danbam.mobile.ui.movie.all.MovieAllScreen
 import com.danbam.mobile.ui.movie.detail.MovieDetailScreen
+import com.danbam.mobile.ui.movie.make.AddActorScreen
+import com.danbam.mobile.ui.movie.make.SearchActorScreen
+import com.danbam.mobile.ui.movie.make.WriteActorScreen
+import com.danbam.mobile.ui.movie.make.WriteIntroduceScreen
 import com.danbam.mobile.ui.movie.play.MoviePlayScreen
 import com.google.accompanist.navigation.animation.composable
 
@@ -12,13 +16,16 @@ sealed class MovieNavigationItem(val route: String) {
     object MovieDetail : MovieNavigationItem("movieDetail")
     object MoviePlay : MovieNavigationItem("moviePlay")
     object MovieAll : MovieNavigationItem("movieAll")
+    object MovieWriteIntroduce : MovieNavigationItem("movieWriteIntroduce")
+    object MovieAddActor : MovieNavigationItem("movieAddActor")
+    object MovieSearchActor : MovieNavigationItem("movieSearchActor")
+    object MovieWriteActor : MovieNavigationItem("movieWriteActor")
 }
 
 @OptIn(ExperimentalAnimationApi::class)
 fun NavGraphBuilder.movieGraph(
     navController: NavHostController,
 ) {
-
     composable(route = MovieNavigationItem.MovieDetail.route) {
         MovieDetailScreen(navController = navController)
     }
@@ -27,5 +34,17 @@ fun NavGraphBuilder.movieGraph(
     }
     composable(route = MovieNavigationItem.MovieAll.route) {
         MovieAllScreen(navController = navController)
+    }
+    composable(route = MovieNavigationItem.MovieWriteIntroduce.route) {
+        WriteIntroduceScreen(navController = navController)
+    }
+    composable(route = MovieNavigationItem.MovieAddActor.route) {
+        AddActorScreen(navController = navController)
+    }
+    composable(route = MovieNavigationItem.MovieWriteActor.route) {
+        WriteActorScreen(navController = navController)
+    }
+    composable(route = MovieNavigationItem.MovieSearchActor.route) {
+        SearchActorScreen(navController = navController)
     }
 }
