ddd3b3e326716a0c89adba72d0b5ca6e7bddc571
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-10 13:50:54 +0900

    tv 탭 컴포넌트화

--- design-system/src/main/java/com/danbam/design_system/component/TabLayout.kt
+++ design-system/src/main/java/com/danbam/design_system/component/TabLayout.kt
@@ -9,9 +9,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -21,11 +21,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.tv.material3.ClickableSurfaceDefaults
+import androidx.tv.material3.ExperimentalTvMaterial3Api
+import androidx.tv.material3.Surface
 import com.danbam.design_system.IndiStrawTheme
 import com.danbam.design_system.R
 import com.danbam.design_system.util.RemoveOverScrollLazyRow
@@ -65,8 +69,7 @@ fun IndiStrawTab(
         TitleSemiBold(
             modifier = modifier
                 .padding(bottom = 5.dp)
-                .wrapContentWidth()
-                .wrapContentHeight()
+                .wrapContentSize()
                 .indiStrawClickable(onClick = onSelect)
                 .onSizeChanged {
                     tabWidth = it.width.toDp(context)
@@ -83,14 +86,60 @@ fun IndiStrawTab(
     }
 }
 
+@OptIn(ExperimentalTvMaterial3Api::class)
+@Composable
+fun IndiStrawTvTab(
+    modifier: Modifier = Modifier,
+    text: String,
+    isSelect: Boolean,
+    onSelect: () -> Unit,
+) {
+    val context = LocalContext.current
+    var tabWidth by remember { mutableStateOf(0) }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Surface(
+            shape = ClickableSurfaceDefaults.shape(
+                shape = RoundedCornerShape(0.dp)
+            ),
+            color = ClickableSurfaceDefaults.color(
+                color = Color.Transparent,
+                focusedColor = Color.Transparent,
+                pressedColor = Color.Transparent,
+                disabledColor = Color.Transparent
+            ),
+            onClick = onSelect
+        ) {
+            ExampleTextMedium(
+                modifier = modifier
+                    .padding(bottom = 5.dp)
+                    .onSizeChanged {
+                        tabWidth = it.width.toDp(context)
+                    },
+                text = text,
+                color = if (isSelect) IndiStrawTheme.colors.white else IndiStrawTheme.colors.darkGray3,
+                fontSize = 35
+            )
+        }
+        if (isSelect) {
+            IndiStrawTabIndicator(
+                tabWidth = (tabWidth + 10).dp,
+                tabHeight = 3.dp
+            )
+        }
+    }
+}
+
 @Composable
 private fun IndiStrawTabIndicator(
     modifier: Modifier = Modifier,
     tabWidth: Dp,
+    tabHeight: Dp = 1.dp
 ) {
     Spacer(
         modifier = modifier
-            .height(1.dp)
+            .height(tabHeight)
             .width(tabWidth)
             .background(
                 color = IndiStrawTheme.colors.main,
