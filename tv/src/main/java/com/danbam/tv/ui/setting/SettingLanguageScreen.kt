cf0dde92ec269ff8dd90ae834c2789ce60fe6107
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-12 21:06:04 +0900

    언어 설정 시 포커싱 설정

--- tv/src/main/java/com/danbam/tv/ui/setting/SettingLanguageScreen.kt
+++ tv/src/main/java/com/danbam/tv/ui/setting/SettingLanguageScreen.kt
@@ -1,76 +1,107 @@
 package com.danbam.tv.ui.setting
 
-import androidx.compose.foundation.BorderStroke
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.tv.foundation.lazy.list.TvLazyColumn
 import androidx.tv.foundation.lazy.list.items
-import androidx.tv.material3.Border
 import androidx.tv.material3.ClickableSurfaceDefaults
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.Surface
 import com.danbam.design_system.IndiStrawTheme
 import com.danbam.design_system.R
-import com.danbam.design_system.component.DialogMedium
-import com.danbam.design_system.component.TitleRegular
+import com.danbam.design_system.component.ExampleTextMedium
+
+sealed class LanguageType(val stringId: Int) {
+    companion object {
+        fun toList() = listOf(Korean, Japanese, Chinese, English)
+    }
+
+    object Korean : LanguageType(R.string.id)
+    object Japanese : LanguageType(R.string.id)
+    object Chinese : LanguageType(R.string.id)
+    object English : LanguageType(R.string.id)
+}
 
 @OptIn(ExperimentalTvMaterial3Api::class)
 @Composable
 fun SettingLanguageScreen(
-
+    selectLanguage: LanguageType,
+    parentFocusRequester: FocusRequester
 ) {
-    Column(
-        modifier = Modifier
-            .padding(start = 40.dp, top = 60.dp, bottom = 100.dp)
-            .fillMaxWidth(0.23F)
-            .fillMaxHeight()
-    ) {
-        DialogMedium(text = stringResource(id = R.string.setting), fontSize = 48)
-        Spacer(modifier = Modifier.height(60.dp))
-        TvLazyColumn {
-            items(SettingNavigation.toList()) {
-                Surface(
-                    modifier = Modifier
-                        .padding(top = if (it == SettingNavigation.Logout) 50.dp else 20.dp)
-                        .fillMaxWidth(),
-                    scale = ClickableSurfaceDefaults.scale(
-                        focusedScale = 1F
-                    ),
-                    color = ClickableSurfaceDefaults.color(
-                        color = Color.Transparent,
-                        focusedColor = Color.Transparent,
-                        pressedColor = Color.Transparent,
-                        disabledColor = Color.Transparent
-                    ),
-                    border = ClickableSurfaceDefaults.border(
-                        focusedBorder = Border(
-                            border = BorderStroke(3.dp, IndiStrawTheme.colors.main),
-                            shape = IndiStrawTheme.shapes.smallRounded
-                        )
-                    ),
-                    onClick = {
-                    }
-                ) {
-                    TitleRegular(
-                        modifier = Modifier.padding(
-                            horizontal = 12.dp,
-                            vertical = 4.dp
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+
+    BackHandler {
+        parentFocusRequester.requestFocus()
+    }
+
+    Row {
+        Column(
+            modifier = Modifier
+                .padding(vertical = 40.dp)
+                .fillMaxWidth(0.4F)
+                .fillMaxHeight()
+        ) {
+            TvLazyColumn(
+                verticalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+                items(LanguageType.toList()) {
+                    Surface(
+                        modifier = Modifier.focusRequester(if (selectLanguage == it) focusRequester else FocusRequester()),
+                        scale = ClickableSurfaceDefaults.scale(
+                            focusedScale = 1F
                         ),
-                        text = stringResource(id = it.stringId),
-                        fontSize = 31,
-                        color = IndiStrawTheme.colors.gray
-                    )
+                        shape = ClickableSurfaceDefaults.shape(
+                            shape = RoundedCornerShape(0.dp)
+                        ),
+                        color = ClickableSurfaceDefaults.color(
+                            color = Color.Transparent,
+                            focusedColor = IndiStrawTheme.colors.main,
+                            pressedColor = IndiStrawTheme.colors.main,
+                            disabledColor = Color.Transparent
+                        ),
+                        onClick = {
+                            parentFocusRequester.requestFocus()
+                        }
+                    ) {
+                        ExampleTextMedium(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 3.dp),
+                            text = stringResource(id = it.stringId), fontSize = 31,
+                            textAlign = TextAlign.Center
+                        )
+                    }
                 }
             }
         }
+        Box(
+            modifier = Modifier
+                .width(3.dp)
+                .fillMaxHeight()
+                .background(IndiStrawTheme.colors.gray)
+        )
     }
 }
