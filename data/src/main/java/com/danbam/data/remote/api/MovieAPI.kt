6a5f8399e1a0c3c64c0ce2e0bc40b25641249518
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-12 23:01:09 +0900

    영화 리스트 조회 API 작성

--- data/src/main/java/com/danbam/data/remote/api/MovieAPI.kt
+++ data/src/main/java/com/danbam/data/remote/api/MovieAPI.kt
@@ -1,4 +1,15 @@
 package com.danbam.data.remote.api
 
+import com.danbam.data.remote.response.MoviePageResponse
+import com.danbam.data.remote.util.EndPoint
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface MovieAPI {
+    @GET(EndPoint.Movie)
+    suspend fun movieList(
+        @Query("page") page: Int = 0,
+        @Query("size") size: Int = 10,
+        @Query("keyword") genre: String? = null
+    ): MoviePageResponse
 }
--- data/src/main/java/com/danbam/data/remote/datasource/MovieRemoteDataSourceImpl.kt
+++ data/src/main/java/com/danbam/data/remote/datasource/MovieRemoteDataSourceImpl.kt
@@ -1,9 +1,23 @@
 package com.danbam.data.remote.datasource
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.danbam.data.remote.api.MovieAPI
+import com.danbam.data.remote.pagingsource.MoviePagingSource
+import com.danbam.data.remote.response.MovieResponse
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class MovieRemoteDataSourceImpl @Inject constructor(
     private val movieAPI: MovieAPI
 ) : MovieRemoteDataSource {
+    override suspend fun movieList(genre: String?): Flow<PagingData<MovieResponse>> =
+        Pager(config = PagingConfig(pageSize = 10),
+            pagingSourceFactory = {
+                MoviePagingSource(
+                    movieAPI = movieAPI,
+                    genre = genre
+                )
+            }).flow
 }
