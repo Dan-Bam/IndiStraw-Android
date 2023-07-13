d2e001fa8590795ffcfe1534cde55686143b5151
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-14 00:39:56 +0900

    티비 검색 기능 구현

--- tv/src/main/java/com/danbam/tv/ui/search/SearchViewModel.kt
+++ tv/src/main/java/com/danbam/tv/ui/search/SearchViewModel.kt
@@ -3,6 +3,7 @@ package com.danbam.tv.ui.search
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.danbam.domain.entity.RecentSearchEntity
+import com.danbam.domain.usecase.movie.MoviePopularListUseCase
 import com.danbam.domain.usecase.search.GetRecentSearchUseCase
 import com.danbam.domain.usecase.search.GetRelatedSearchUseCase
 import com.danbam.domain.usecase.search.SearchMovieUseCase
@@ -18,7 +19,8 @@ import javax.inject.Inject
 class SearchViewModel @Inject constructor(
     private val getRecentSearchUseCase: GetRecentSearchUseCase,
     private val getRelatedSearchUseCase: GetRelatedSearchUseCase,
-    private val searchMovieUseCase: SearchMovieUseCase
+    private val searchMovieUseCase: SearchMovieUseCase,
+    private val moviePopularListUseCase: MoviePopularListUseCase
 ) : ContainerHost<SearchState, Unit>, ViewModel() {
     override val container = container<SearchState, Unit>(SearchState())
 
@@ -38,9 +40,19 @@ class SearchViewModel @Inject constructor(
         }
     }
 
+    fun moviePopularList() = intent {
+        viewModelScope.launch {
+            moviePopularListUseCase().onSuccess {
+
+            }
+        }
+    }
+
     fun search(keyword: String) = intent {
         viewModelScope.launch {
-            searchMovieUseCase(recentSearchEntity = RecentSearchEntity(search = keyword))
+            searchMovieUseCase(recentSearchEntity = RecentSearchEntity(search = keyword)).onSuccess {
+                reduce { state.copy(resultPager = it) }
+            }
         }
     }
 }
