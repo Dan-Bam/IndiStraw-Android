18bac2d18aedd317ab0df1e1a1c7aba821d1cdc5
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-13 17:26:03 +0900

    티비 메인 페이지 조회 기능 구현

--- tv/src/main/java/com/danbam/tv/ui/home/HomeContract.kt
+++ tv/src/main/java/com/danbam/tv/ui/home/HomeContract.kt
@@ -1,5 +1,8 @@
 package com.danbam.tv.ui.home
 
+import com.danbam.domain.entity.MovieEntity
+
 data class HomeState(
     val currentMovieIndex: Int = 0,
+    val movieList: List<MovieEntity> = listOf(),
 )
