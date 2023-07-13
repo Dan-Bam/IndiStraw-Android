d229b1f29613bbd1b70987a1e4a589c23d290047
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-13 09:52:59 +0900

    후원한 리스트 API 연결

--- domain/src/main/java/com/danbam/domain/repository/FundingRepository.kt
+++ domain/src/main/java/com/danbam/domain/repository/FundingRepository.kt
@@ -1,8 +1,10 @@
 package com.danbam.domain.repository
 
+import com.danbam.domain.entity.FundingEntity
 import com.danbam.domain.param.FundingParam
 
 interface FundingRepository {
     suspend fun getReceipt(): String
     suspend fun funding(crowdfundingIdx: Long, rewardIdx: Long, fundingParam: FundingParam)
+    suspend fun fundingList(): List<FundingEntity>
 }
