3bb603603c7ce516cb2f38159a23df3e931a13c3
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-13 22:58:55 +0900

    중국어 string 추가

--- design-system/src/main/java/com/danbam/design_system/util/SystemUtil.kt
+++ design-system/src/main/java/com/danbam/design_system/util/SystemUtil.kt
@@ -7,13 +7,14 @@ import java.util.Locale
 
 sealed class Language(val type: String, val stringId: Int) {
     companion object {
-        fun toList() = listOf(English, Korean, Japanese, Spanish)
+        fun toList() = listOf(English, Korean, Japanese, Spanish, Chinese)
     }
 
     object English : Language("en", R.string.english)
     object Korean : Language("kr", R.string.korean)
     object Japanese : Language("ja", R.string.japanese)
     object Spanish : Language("es", R.string.spanish)
+    object Chinese : Language("zh", R.string.chinese)
 }
 
 fun Language.changeLanguage(context: Context) {
