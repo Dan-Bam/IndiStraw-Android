32de11e78f929b6819084cca25ed802376898661
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-13 16:45:04 +0900

    영화 url 키 추가

--- mobile/src/main/java/com/danbam/mobile/ui/movie/navigation/MovieGraph.kt
+++ mobile/src/main/java/com/danbam/mobile/ui/movie/navigation/MovieGraph.kt
@@ -28,6 +28,7 @@ sealed class MovieNavigationItem(val route: String) {
 object MovieDeepLinkKey {
     const val ADD_ACTOR_TYPE = "addActorType"
     const val MOVIE_INDEX = "movieIndex"
+    const val MOVIE_URL = "movieUrl"
 }
 
 object ActorType {
@@ -52,8 +53,17 @@ fun NavGraphBuilder.movieGraph(
         val movieIndex = it.arguments?.getInt(MovieDeepLinkKey.MOVIE_INDEX) ?: 0
         MovieDetailScreen(navController = navController, movieIndex = movieIndex)
     }
-    composable(route = MovieNavigationItem.Play.route) {
-        MoviePlayScreen()
+    composable(
+        route = MovieNavigationItem.Play.route
+            + MovieDeepLinkKey.MOVIE_URL + "{${MovieDeepLinkKey.MOVIE_INDEX}}",
+        arguments = listOf(
+            navArgument(MovieDeepLinkKey.MOVIE_URL) {
+                type = NavType.StringType
+            }
+        )
+    ) {
+        val movieUrl = it.arguments?.getString(MovieDeepLinkKey.MOVIE_URL) ?: ""
+        MoviePlayScreen(movieUrl = movieUrl)
     }
     composable(route = MovieNavigationItem.All.route) {
         MovieAllScreen(navController = navController)
