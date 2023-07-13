48158dc7a618e00fb25c573a242db236deec1abc
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-13 09:02:39 +0900

    영화 소개 정보 저장

---
+++ mobile/src/main/java/com/danbam/mobile/ui/movie/make/MakeMovieState.kt
@@ -0,0 +1,10 @@
+package com.danbam.mobile.ui.movie.make
+
+data class MakeMovieState(
+    val thumbnailUrl: String? = null,
+    val movieUrl: String? = null,
+    val title: String = "",
+    val description: String = "",
+    val isFunding: Boolean = false,
+    val imageList: List<String> = listOf()
+)
--- mobile/src/main/java/com/danbam/mobile/ui/movie/make/MakeMovieViewModel.kt
+++ mobile/src/main/java/com/danbam/mobile/ui/movie/make/MakeMovieViewModel.kt
@@ -3,10 +3,12 @@ package com.danbam.mobile.ui.movie.make
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.danbam.domain.usecase.file.SendFileUseCase
+import com.danbam.mobile.util.android.errorHandling
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.reduce
 import org.orbitmvi.orbit.viewmodel.container
 import java.io.File
 import javax.inject.Inject
@@ -14,14 +16,42 @@ import javax.inject.Inject
 @HiltViewModel
 class MakeMovieViewModel @Inject constructor(
     private val sendFileUseCase: SendFileUseCase,
-) : ContainerHost<Unit, Unit>, ViewModel() {
-    override val container = container<Unit, Unit>(Unit)
+) : ContainerHost<MakeMovieState, Unit>, ViewModel() {
+    override val container = container<MakeMovieState, Unit>(MakeMovieState())
 
     fun uploadFile(file: File, onUploaded: (String) -> Unit) = intent {
         viewModelScope.launch {
             sendFileUseCase(file = file).onSuccess {
                 onUploaded(it.file)
+            }.onFailure {
+                it.errorHandling(unknownAction = {})
             }
+        }
+    }
+
+    fun saveIntroduce(
+        thumbnailUrl: String?,
+        movieUrl: String?,
+        title: String,
+        description: String,
+        isFunding: Boolean,
+        onNext: () -> Unit
+    ) = intent {
+        if (thumbnailUrl.isNullOrBlank()) return@intent
+        else if (movieUrl.isNullOrBlank()) return@intent
+        else if (title.isEmpty()) return@intent
+        else if (description.isEmpty()) return@intent
+        else {
+            reduce {
+                state.copy(
+                    thumbnailUrl = thumbnailUrl,
+                    movieUrl = movieUrl,
+                    title = title,
+                    description = description,
+                    isFunding = isFunding
+                )
+            }
+            onNext()
         }
     }
 }
