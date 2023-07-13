c67e56dbb5270ff295a4948ddaaaa5e1baa90162
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-07-13 09:02:39 +0900

    영화 소개 정보 저장

--- mobile/src/main/java/com/danbam/mobile/ui/movie/make/MakeMovieViewModel.kt
+++ mobile/src/main/java/com/danbam/mobile/ui/movie/make/MakeMovieViewModel.kt
@@ -8,8 +8,6 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
-import org.orbitmvi.orbit.syntax.simple.postSideEffect
-import org.orbitmvi.orbit.syntax.simple.reduce
 import org.orbitmvi.orbit.viewmodel.container
 import java.io.File
 import javax.inject.Inject
