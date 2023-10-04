7f82808c931515938c0df4207c7a534aaefaedce
백승민 <bsmin0417@naver.com>
2023-10-04 14:04:22 +0900

    file에서 imageUrl로 네이밍 변경

--- data/src/main/java/com/danbam/data/remote/response/FileResponse.kt
+++ data/src/main/java/com/danbam/data/remote/response/FileResponse.kt
@@ -4,10 +4,10 @@ import com.danbam.domain.entity.FileEntity
 import com.google.gson.annotations.SerializedName
 
 data class FileResponse(
-    @SerializedName("file")
+    @SerializedName("imageUrl")
     val file: String,
 )
 
 fun FileResponse.toEntity() = FileEntity(
-    file = file
+    file = "https://indistraw-s3.s3.ap-northeast-2.amazonaws.com/$file"
 )
