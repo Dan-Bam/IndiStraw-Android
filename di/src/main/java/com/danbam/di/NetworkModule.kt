b64380465be49620189226741b75f246546d806a
백승민 <bsmin0417@naver.com>
2023-10-06 13:59:13 +0900

    HTTP 캐싱 설정

--- di/src/main/java/com/danbam/di/NetworkModule.kt
+++ di/src/main/java/com/danbam/di/NetworkModule.kt
@@ -1,5 +1,6 @@
 package com.danbam.di
 
+import android.content.Context
 import android.util.Log
 import com.danbam.data.remote.api.AccountAPI
 import com.danbam.data.remote.api.AddressAPI
@@ -15,7 +16,9 @@ import com.danbam.data.remote.interceptor.IndiStrawInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -26,10 +29,15 @@ import retrofit2.converter.gson.GsonConverterFactory
 object NetworkModule {
     @Provides
     fun provideOkHttpclient(
-        indiStrawInterceptor: IndiStrawInterceptor,
-    ): OkHttpClient = OkHttpClient.Builder()
-        .addInterceptor(indiStrawInterceptor)
-        .build()
+        @ApplicationContext context: Context,
+        indiStrawInterceptor: IndiStrawInterceptor
+    ): OkHttpClient {
+        IndiStrawInterceptor.context = context
+        return OkHttpClient.Builder()
+            .cache(Cache(context.cacheDir, 20L * 1024 * 1024))
+            .addInterceptor(indiStrawInterceptor)
+            .build()
+    }
 
     @Provides
     fun provideRetrofitClient(
