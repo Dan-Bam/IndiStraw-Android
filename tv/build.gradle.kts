ab76425644a191eb1534c05d76cc21edf380a472
백승민 <bsmin0417@naver.com>
2023-10-11 13:11:22 +0900

    불필요한 gradle 제거

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -68,8 +68,7 @@ dependencies {
     implementation(project(":domain"))
     implementation(project(":di"))
     implementation(project(":design-system"))
-
-
+    
     implementation(Dependency.AndroidX.CORE_KTX)
     implementation(Dependency.AndroidX.LIFECYCLE)
     implementation(Dependency.AndroidX.PAGING_COMPOSE)
@@ -95,9 +94,6 @@ dependencies {
     implementation(Dependency.Mvi.ORBIT_TEST)
 
     implementation(Dependency.Coil.COIL)
-
-    implementation(Dependency.ExoPlayer.EXO_PLAYER_CORE)
-    implementation(Dependency.ExoPlayer.EXO_PLAYER_UI)
 
     implementation(Dependency.Tv.MATERIAL)
     implementation(Dependency.Tv.FOUNDATION)
