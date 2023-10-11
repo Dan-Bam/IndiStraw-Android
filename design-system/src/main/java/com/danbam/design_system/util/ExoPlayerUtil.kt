ebb1e210e27b9f03e8e75364c6b9d1b08e53f6dc
백승민 <bsmin0417@naver.com>
2023-10-11 17:55:13 +0900

    KeyEvent Extension 구현

---
+++ design-system/src/main/java/com/danbam/design_system/util/ExoPlayerUtil.kt
@@ -0,0 +1,48 @@
+package com.danbam.design_system.util
+
+import android.view.KeyEvent
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.Player
+
+fun ExoPlayer.detectKeyEvent(event: KeyEvent): Boolean {
+    val keyCode: Int = event.keyCode
+    if (event.action == KeyEvent.ACTION_DOWN) {
+        if (keyCode == KeyEvent.KEYCODE_MEDIA_FAST_FORWARD) {
+            if (playbackState != Player.STATE_ENDED) {
+                seekForward()
+            }
+        } else if (keyCode == KeyEvent.KEYCODE_MEDIA_REWIND) {
+            seekBack()
+        } else if (event.repeatCount == 0) {
+            when (keyCode) {
+                KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE, KeyEvent.KEYCODE_HEADSETHOOK -> dispatchPlayPause()
+                KeyEvent.KEYCODE_MEDIA_PLAY -> dispatchPlay()
+                KeyEvent.KEYCODE_MEDIA_PAUSE -> pause()
+                KeyEvent.KEYCODE_MEDIA_NEXT -> seekToNext()
+                KeyEvent.KEYCODE_MEDIA_PREVIOUS -> seekToPrevious()
+                else -> {}
+            }
+        }
+    }
+    return true
+}
+
+private fun ExoPlayer.dispatchPlayPause() {
+    val state = playbackState
+    if (state == Player.STATE_IDLE || state == Player.STATE_ENDED || !playWhenReady) {
+        dispatchPlay()
+    } else {
+        pause()
+    }
+}
+
+private fun ExoPlayer.dispatchPlay() {
+    val state = playbackState
+    if (state == Player.STATE_IDLE) {
+        prepare()
+    } else if (state == Player.STATE_ENDED) {
+        seekTo(currentMediaItemIndex, C.TIME_UNSET)
+    }
+    play()
+}
