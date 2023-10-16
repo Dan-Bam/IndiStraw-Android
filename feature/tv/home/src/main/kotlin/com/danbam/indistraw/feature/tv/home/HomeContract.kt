4f6f380e0c296bc5572f5ddf7e3162814e70e1e1
백승민 <bsmin0417@naver.com>
2023-10-17 08:37:20 +0900

    itemFocusing 문제 해결

--- feature/tv/home/src/main/kotlin/com/danbam/indistraw/feature/tv/home/HomeContract.kt
+++ feature/tv/home/src/main/kotlin/com/danbam/indistraw/feature/tv/home/HomeContract.kt
@@ -4,7 +4,7 @@ import com.danbam.indistraw.core.domain.entity.banner.BannerEntity
 import com.danbam.indistraw.core.domain.entity.movie.MovieEntity
 
 data class HomeState(
-    val currentMovieIndex: Long = 0,
+    val currentMovieIndex: Long? = null,
     val movieList: List<MovieEntity> = listOf(),
     val bannerList: List<BannerEntity> = listOf()
 )
